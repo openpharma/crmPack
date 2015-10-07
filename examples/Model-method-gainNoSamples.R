57b7bfda828caca6904d6c3d09edb9e949fee2ec
Winnie Yeung <w.yeung1@lancaster.ac.uk>
2015-10-07 14:49:42 +0100

    Fixed an error in example model-method-gainNoSamples

--- examples/Model-method-gainNoSamples.R
+++ examples/Model-method-gainNoSamples.R
@@ -1,6 +1,10 @@
 ##Obtain the gain value for a given dose, a pseudo DLE model and  a pseudo efficacy model
 ## without samples
 ##The DLE model must be from 'ModelTox' class (DLEmodel slot)
+emptydata<- DataDual(doseGrid=seq(25,300,25))
+data<-emptydata
+DLEmodel<-LogisticIndepBeta(binDLE=c(1.05,1.8),DLEweights=c(3,3),DLEdose=c(25,300),data=data)
 ##The efficacy model must be from 'Effloglog' class  (Effmodel slot)
+Effmodel<-Effloglog(Eff=c(1.223,2.513),Effdose=c(25,300),nu=c(a=0.025,b=1),data=data)
 ## Given a dose level 75,
 gain(dose=75,DLEmodel=DLEmodel,Effmodel=Effmodel)
