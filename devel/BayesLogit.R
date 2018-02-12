f79cfb02e8fdaf9a94491b5dedd227ed483db68d
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2018-02-12 18:18:26 +0100

    include Winnie's example, which shows that MCMClogit fails for linearly separable input data unfortunately.

--- devel/BayesLogit.R
+++ devel/BayesLogit.R
@@ -42,3 +42,42 @@ for(i in 1:3)
 }
 
 ## so this looks comparable - ok
+
+## Winnie's example:
+priorphi1 <- -1.946152
+priorphi2 <- 0.4122909
+precision <- matrix(c(1.402500, 6.303606,
+                      6.303606, 30.495350),
+                    nrow=2,
+                    byrow=TRUE)
+
+data<-Data(x=c(25,50,50,75,100,100,225,300),y=c(1,0,0,0,1,1,1,0),  doseGrid=seq(25,300,25))
+
+model<-LogisticIndepBeta(binDLE=c(1.05,1.8),DLEweights=c(3,3),DLEdose=c(25,300),data=data)
+
+options<-McmcOptions(burnin=100,step=2,samples=200)
+
+## The old code for package BayesLogit  for InitRes was
+
+X <- cbind(1, log(data@x))
+initRes <- BayesLogit::logit(y=data@y,
+                             X=X,
+                             m0=c(priorphi1,priorphi2),
+                             P0=precision,
+                             samp=sampleSize(options),
+                             burn=options@burnin)
+samples <- initRes$beta
+head(samples)
+
+tempData <- data.frame(y=data@y,
+                       logx=log(data@x))
+initRes2 <- MCMCpack::MCMClogit(formula = y ~ logx,
+                               data=tempData,
+                               b0=c(priorphi1,priorphi2), BO=precision, 
+                               mcmc=options@iterations - options@burnin,
+                               burnin=options@burnin,
+                               thin=options@step,
+                               seed=19,
+                               beta.start=c(0, 0))
+head(initRes2)
+hist(initRes2[, 1])
