e5ca77916a042dcd2f12e54c7b4bc47bb37fbaef
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2021-11-15 14:51:12 +0100

    address review comments

--- examples/design-method-examine-DADesign.R
+++ examples/design-method-examine-DADesign.R
@@ -1,7 +1,7 @@
 # Define the dose-grid and PEM parameters
 emptydata <- DataDA(doseGrid=c(0.1, 0.5,1, 1.5, 3, 6,
                                seq(from=10, to=80, by=2)),Tmax=60)
-# Initialize the mDA-CRM model 
+# Initialize the mDA-CRM model
 npiece_=10
 Tmax_=60
 
@@ -18,14 +18,14 @@ model<-DALogisticLogNormal(mean=c(-0.85,1),
 # Choose the rule for dose increments
 myIncrements <- IncrementsRelative(intervals=c(0,20),
                                    increments=c(1,0.33))
-# Choose the rule for selecting the next dose 
+# Choose the rule for selecting the next dose
 nextMaxDose <- maxDose(myIncrements,data=emptydata)
 
 myNextBest <- NextBestNCRM(target=c(0.2,0.35),
                            overdose=c(0.35,1),
                            maxOverdoseProb=0.25)
 
-# Choose the rule for the cohort-size 
+# Choose the rule for the cohort-size
 mySize1 <- CohortSizeRange(intervals=c(0, 30),
                            cohortSize=c(1, 3))
 mySize2 <- CohortSizeDLT(DLTintervals=c(0, 1),
@@ -39,7 +39,7 @@ myStopping2 <- StoppingMinPatients(nPatients=50)
 
 myStopping <- (myStopping1 | myStopping2)
 
-# Choose the safety window 
+# Choose the safety window
 mysafetywindow=SafetyWindowConst(c(6,2),7,7)
 
 # Initialize the design
@@ -57,4 +57,4 @@ set.seed(4235)
 # increased for a real case.
 # This procedure will take a while.
 options <- McmcOptions(burnin=10,step=1,samples=100)
-examine(design, mcmcOptions=options)
+# examine(design, mcmcOptions=options)
