14a4601e6aee2241bebcfe6a80527c586d0b9837
Wojtek <wojciech.wojciak@contractors.roche.com>
2021-12-01 16:20:07 +0100

    Closes #36
    Closes #36

---
+++ tests/testthat/test-Data-methods.R
@@ -0,0 +1,9 @@
+# as.list ----
+
+test_that("Coercion creates the expected `List` object", {
+  object <- Data()
+  result <- as.list(object)
+
+  expect_class(result, "list")
+  expect_identical(slotNames(object), names(result))
+})
