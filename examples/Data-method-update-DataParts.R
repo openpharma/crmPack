0895cb6638c88f3196ec1520c277c3b570059234
Wojciech Wójciak <11532997+wwojciech@users.noreply.github.com>
2021-12-09 17:15:26 +0100

    Closes #44 (#49)
    * Closes #44
    
    Closes #44
    
    * Closes #44
    
    Shorter lines for linter
    
    * Closes #44
    
    Lintr update
    
    * Closes #44
    
    Update of params to update-DataParts.
    
    * Closes #44
    
    SuperLint update - shortening the lines

--- examples/Data-method-update-DataParts.R
+++ examples/Data-method-update-DataParts.R
@@ -1,18 +1,14 @@
+# Create an object of class 'DataParts'.
+my_data <- DataParts(
+  x = c(0.1, 0.5, 1.5),
+  y = c(0, 0, 0),
+  doseGrid = c(0.1, 0.5, 1.5, 3, 6, seq(from = 10, to = 80, by = 2)),
+  part = c(1L, 1L, 1L),
+  nextPart = 1L,
+  part1Ladder = c(0.1, 0.5, 1.5, 3, 6, 10)
+)
 
-# create an object of class 'DataParts'
-myData <- DataParts(x=c(0.1,0.5,1.5),
-                    y=c(0,0,0),
-                    doseGrid=c(0.1,0.5,1.5,3,6,
-                               seq(from=10,to=80,by=2)),
-                    part=c(1L,1L,1L),
-                    nextPart=1L,
-                    part1Ladder=c(0.1,0.5,1.5,3,6,10))
-
-# update the data with a new cohort
-# to be noted that since we reached the last level from part1Ladder then
-# nextPart is switched from '1' to '2'
-myData <- update(myData,
-                 x=10,
-                 y=c(0))
-
-
+# Update the data with a new cohort.
+# Note that since we reached the last level from 'part1Ladder'
+# then the 'nextPart' is switched from '1' to '2'.
+my_data1 <- update(my_data, x = 10, y = 0L)
--- tests/testthat/test-Data-methods.R
+++ tests/testthat/test-Data-methods.R
@@ -24,11 +24,13 @@ test_that("Plot works as expected for Data object with placebo and blinding", {
   vdiffr::expect_doppelganger("Plot of Data with placebo and blinding", result)
 })
 
-test_that("Plot works as expected for Data object with placebo, blinding and no legend", {
+test_that("Plot works for Data object with placebo, blinding and no legend", {
   data <- h_get_data()
   result <- plot(data, blind = TRUE, legend = FALSE)
 
-  vdiffr::expect_doppelganger("Plot of Data with placebo, blinding and no legend", result)
+  vdiffr::expect_doppelganger(
+    "Plot of Data with placebo, blinding and no legend", result
+  )
 })
 
 # plot-DataDual ----
@@ -40,11 +42,13 @@ test_that("Plot works as expected for DataDual object with placebo", {
   vdiffr::expect_doppelganger("Plot of DataDual with placebo", result)
 })
 
-test_that("Plot works as expected for DataDual object with placebo and blinding", {
+test_that("Plot works for DataDual object with placebo and blinding", {
   data <- h_get_data_dual()
   result <- plot(data, blind = TRUE)
 
-  vdiffr::expect_doppelganger("Plot of DataDual with placebo and blinding", result)
+  vdiffr::expect_doppelganger(
+    "Plot of DataDual with placebo and blinding", result
+  )
 })
 
 # plot-DataDA ----
@@ -56,11 +60,13 @@ test_that("Plot works as expected for DataDA object with placebo", {
   vdiffr::expect_doppelganger("Plot of DataDA with placebo", result)
 })
 
-test_that("Plot works as expected for DataDA object with placebo and blinding", {
+test_that("Plot works for DataDA object with placebo and blinding", {
   data <- h_get_data_augmented()
   result <- plot(data, blind = TRUE)
 
-  vdiffr::expect_doppelganger("Plot of DataDA with placebo and blinding", result)
+  vdiffr::expect_doppelganger(
+    "Plot of DataDA with placebo and blinding", result
+  )
 })
 
 # update-Data ----
@@ -80,12 +86,12 @@ test_that("Update of Data works as expected", {
   expect_equal(result, object)
 })
 
-test_that("Update of Data works as expected when doses are added to the old cohort", {
+test_that("Update of Data works when doses are added to the old cohort", {
   object <- h_get_data()
   result <- update(object, x = 100, y = c(0L, 1L, 1L), new_cohort = FALSE)
 
   object@x <- c(object@x, 100, 100, 100)
-  object@y <- c(object@y, c(0L, 1L, 1L))
+  object@y <- c(object@y, 0L, 1L, 1L)
   object@nObs <- object@nObs + 3L
   object@ID <- c(object@ID, 13L, 14L, 15L)
   object@xLevel <- c(object@xLevel, 5L, 5L, 5L)
@@ -101,4 +107,71 @@ test_that("Update of Data throws the error for a dose x out of the grid", {
     update(object, x = 12345, y = c(0L, 1L, 1L), new_cohort = FALSE),
     ".*Dose values in x must be from doseGrid.*"
   )
+})
+
+test_that("Update of Data, no error for non-valid update and validation off", {
+  object <- h_get_data()
+  expect_silent(
+    update(
+      object,
+      x = 12345, y = c(0L, 1L, 1L), new_cohort = FALSE, check = FALSE
+    )
+  )
+})
+
+# update-DataParts ----
+
+test_that("Update of DataParts works as expected", {
+  object <- h_get_data_parts() # nextPart equals 1L here.
+  result <- update(object, x = 200, y = c(0L, 1L))
+
+  object@x <- c(object@x, 200, 200)
+  object@y <- c(object@y, 0L, 1L)
+  object@nObs <- object@nObs + 2L
+  object@ID <- c(object@ID, 13L, 14L)
+  object@xLevel <- c(object@xLevel, 9L, 9L)
+  object@cohort <- c(object@cohort, 4L, 4L)
+  object@part <- c(object@part, 1L, 1L)
+  object@nextPart <- 2L
+
+  expect_valid(result, "DataParts")
+  expect_equal(result, object)
+})
+
+test_that("Update of DataParts works as expected", {
+  object <- h_get_data_parts()
+  # The above object has nextPart slot equals 1L and y not all equal 0.
+  result <- update(object, x = 200, y = c(0L, 1L))
+
+  object@x <- c(object@x, 200, 200)
+  object@y <- c(object@y, 0L, 1L)
+  object@nObs <- object@nObs + 2L
+  object@ID <- c(object@ID, 13L, 14L)
+  object@xLevel <- c(object@xLevel, 9L, 9L)
+  object@cohort <- c(object@cohort, 4L, 4L)
+  object@part <- c(object@part, 1L, 1L)
+  object@nextPart <- 2L
+
+  expect_valid(result, "DataParts")
+  expect_equal(result, object)
+})
+
+test_that("Update of DataParts works, no DLT and x eq max of part1Ladder", {
+  object <- h_get_data_parts()
+  object@nextPart <- 1L
+  object@y <- rep(0L, 12)
+
+  result <- update(object, x = 250, y = c(0L, 0L)) # max of part1Ladder eq. 250.
+
+  object@x <- c(object@x, 250, 250)
+  object@y <- c(object@y, 0L, 0L)
+  object@nObs <- object@nObs + 2L
+  object@ID <- c(object@ID, 13L, 14L)
+  object@xLevel <- c(object@xLevel, 11L, 11L)
+  object@cohort <- c(object@cohort, 4L, 4L)
+  object@part <- c(object@part, 1L, 1L)
+  object@nextPart <- 2L
+
+  expect_valid(result, "DataParts")
+  expect_equal(result, object)
 })
