2f52fbe9eb34bbe01101c151872af61506db8f6d
Wojtek <wojciech.wojciak@contractors.roche.com>
2021-12-17 15:33:20 +0100

    Closes #56
    Minor style update for unit tests.

--- tests/testthat/test-Model-class.R
+++ tests/testthat/test-Model-class.R
@@ -8,49 +8,33 @@ test_that(".AllModels works as expected", {
 # GeneralModel-class ----
 
 test_that(".GeneralModel works as expected", {
+  # nolint start
   result <- expect_silent(.GeneralModel(
-    datamodel = function(x) {
-      x
-    },
-    priormodel = function(x) {
-      x
-    },
-    modelspecs = function(x) {
-      x
-    },
-    init = function(x) {
-      x
-    },
+    datamodel = function(x) {},
+    priormodel = function(x) {},
+    modelspecs = function(x) {},
+    init = function(x) {},
     sample = "param1",
     datanames = "x"
   ))
+  # nolint end
   expect_valid(result, "GeneralModel")
 })
 
 # Model-class ----
 
 test_that(".Model works as expected", {
+  # nolint start
   result <- expect_silent(.Model(
-    dose = function(prob, param1) {
-      prob
-    },
-    prob = function(dose, param1) {
-      dose
-    },
-    datamodel = function(x) {
-      x
-    },
-    priormodel = function(x) {
-      x
-    },
-    modelspecs = function(x) {
-      x
-    },
-    init = function(x) {
-      x
-    },
+    dose = function(prob, param1) {},
+    prob = function(dose, param1) {},
+    datamodel = function(x) {},
+    priormodel = function(x) {},
+    modelspecs = function(x) {},
+    init = function(x) {},
     sample = "param1",
     datanames = "x"
   ))
+  # nolint end
   expect_valid(result, "Model")
 })
