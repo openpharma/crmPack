5bf05bb1adbf2cb04cd625c21e36c9e6284713de
Wojtek <wojciech.wojciak@contractors.roche.com>
2022-01-25 12:57:31 +0100

    Closes #87
    Closes #87

--- tests/testthat/helper-model.R
+++ tests/testthat/helper-model.R
@@ -42,6 +42,14 @@ h_get_model <- function() {
   )
 }
 
+h_get_logistic_normal <- function() {
+  LogisticNormal(
+    mean = c(-0.85, 1),
+    cov = matrix(c(1, -0.5, -0.5, 1), nrow = 2),
+    refDose = 50
+  )
+}
+
 h_get_logistic_log_normal <- function() {
   LogisticLogNormal(
     mean = c(-0.85, 1),
@@ -50,10 +58,10 @@ h_get_logistic_log_normal <- function() {
   )
 }
 
-h_get_logistic_normal <- function() {
-  LogisticNormal(
-    mean = c(-0.85, 1),
-    cov = matrix(c(1, -0.5, -0.5, 1), nrow = 2),
-    refDose = 50
+h_get_logistic_log_normal_sub <- function() {
+  LogisticLogNormalSub(
+    mean = c(1, 5),
+    cov = diag(4, ncol = 2, nrow = 2),
+    refDose = 2
   )
 }
