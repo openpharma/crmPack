8572f27b12fe73039dee6b7d4310470097625b3e
Oliver Boix <oliver.boix@bayer.com>
2022-01-28 13:33:50 +0100

    add unit tests for the rules class

---
+++ tests/testthat/test-Rules-class.R
@@ -0,0 +1,13 @@
+# StoppingMTDCV-class ----
+
+test_that(".StoppingMTDCV works as expected", {
+  result <- expect_silent(.StoppingMTDCV())
+  expect_valid(result, "StoppingMTDCV")
+})
+
+# StoppingMTDCV-constructor ----
+
+test_that("StoppingMTDCV object can be created with user constructor", {
+  result <- expect_silent(StoppingMTDCV())
+  expect_valid(result, "StoppingMTDCV")
+})
