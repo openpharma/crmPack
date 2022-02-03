b7ebd4ff3f2a95ccf615048b65cbdeae1764f832
dimitris.kontos <95762385+dimitriskontosClinBAY@users.noreply.github.com>
2022-02-03 16:10:24 +0200

    added validation funtion validate_increments_numdoselevels for slots of class IncrementsNumDoseLevels

--- R/Rules-validity.R
+++ R/Rules-validity.R
@@ -1,27 +1,30 @@
-#' Internal Helper Functions for Validation of [`Stopping`] Objects
+#' Internal Helper Functions for Validation of [`Increments`] Objects
 #'
 #' @description `r lifecycle::badge("stable")`
 #'
 #' These functions are only used internally to validate the format of an input
-#' [`Stopping`] or inherited classes and therefore not exported.
+#' [`Increments`] or inherited classes and therefore not exported.
 #'
-#' @name validate_stopping
-#' @param object (`Stopping`)\cr object to validate.
+#' @name validate_increments
+#' @param object (`Increments`)\cr object to validate.
 #' @return A `character` vector with the validation failure messages,
 #'   or `TRUE` in case validation passes.
 NULL
 
-#' @describeIn validate_stopping validates that the [`StoppingMTDCV`] object
-#'   contains valid probability target and percentage threshold.
-validate_stopping_mtd_cv <- function(object) {
+#' @describeIn validate_increments validates that the [`IncrementsNumDoseLevels`] object
+#'   contains valid maxLevels and basisLevel option.
+validate_increments_numdoselevels <- function(object) {
   o <- Validate()
   o$check(
-    is.probability(object@target, bounds = FALSE),
-    "target must be probability > 0 and < 1"
+    is.scalar(object@maxLevels) && 
+    is.integer(object@maxLevels) && 
+    object@maxLevels > 0,
+    "maxLevels must be scalar positive integer"
   )
   o$check(
-    is.probability(object@thresh_cv / 100, bounds = FALSE),
-    "thresh_cv must be percentage > 0"
+    is.scalar(object@basisLevel) && 
+    object@basisLevel %in% c("lastGiven", "maxGiven"),
+    "basisLevel must be either 'lastGiven' or 'maxGiven'"
   )
   o$result()
 }
