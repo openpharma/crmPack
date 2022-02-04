c0d12f50566bb427477a7439b8f1e25428c911f4
dimitris.kontos <95762385+dimitriskontosClinBAY@users.noreply.github.com>
2022-02-04 15:35:41 +0200

    intial version for IncrementsNumDoseLevels

---
+++ R/test-Rules-validity.R
@@ -0,0 +1,44 @@
+# validate_increments_numdoselevels ----
+
+test_that("validate_increments_numdoselevels passes for valid object", {
+  object <- IncrementsNumDoseLevels(maxLevels = 1, basisLevel = "lastGiven")
+  expect_true(validate_increments_numdoselevels(object))
+})
+
+test_that("validate_increments_numdoselevels passes for valid object", {
+  object <- IncrementsNumDoseLevels(maxLevels = 1, basisLevel = "maxGiven")
+  expect_true(validate_increments_numdoselevels(object))
+})
+
+test_that("validate_increments_numdoselevels passes for valid object", {
+  object <- IncrementsNumDoseLevels(maxLevels = 1)
+  expect_true(validate_increments_numdoselevels(object))
+})
+
+test_that("validate_increments_numdoselevels returns expected messages for non-valid object", {
+  object <- IncrementsNumDoseLevels()
+  object@maxLevels <- c(-1)
+  object@basisLevel <- c(2)
+  
+  expect_equal(
+    validate_increments_numdoselevels(object),
+    c(
+      "maxLevels must be scalar positive integer",
+      "basisLevel must be either 'lastGiven' or 'maxGiven'"
+    )
+  )
+})
+
+test_that("validate_increments_numdoselevels returns expected messages for non-valid object", {
+  object <- StoppingMTDCV()
+  object@target <- c(3)
+  object@thresh_cv <- c("minGiven")
+  
+  expect_equal(
+    validate_stopping_mtd_cv(object),
+    c(
+      "maxLevels must be scalar positive integer",
+      "basisLevel must be either 'lastGiven' or 'maxGiven'"
+    )
+  )
+})
