90db2db864f9b00f5fc9ff9ab7c725df4fe7990c
dimitris.kontos <95762385+dimitriskontosClinBAY@users.noreply.github.com>
2022-02-08 10:41:45 +0200

    Put spaces around all infix operators. Do not place spaces around code in parentheses or square brackets.

--- tests/testthat/test-Rules-methods.R
+++ tests/testthat/test-Rules-methods.R
@@ -1,10 +1,10 @@
 # maxDose-IncrementsNumDoseLevels ----
 
 # Sample data to test maxDose of IncrementsNumDoseLevels method.
-my_data <- Data(x = c(0.1, 0.5, 1.5, 3, 6, 8, 8, 8, 12, 12, 12, 16, 16, 16, 10, 10, 10 ),
+my_data <- Data(x = c(0.1, 0.5, 1.5, 3, 6, 8, 8, 8, 12, 12, 12, 16, 16, 16, 10, 10, 10),
                 y = c(0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0),
                 cohort = c(0, 1, 2, 3, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8),
-                doseGrid = c(0.1, 0.5, 1.5, 3, 6, 8, seq(from=10, to=80, by=2))
+                doseGrid = c(0.1, 0.5, 1.5, 3, 6, 8, seq(from = 10, to = 80, by = 2))
                 )
 
 test_that("IncrementsNumDoseLevels works correctly if basislevel 'last' is defined", {
