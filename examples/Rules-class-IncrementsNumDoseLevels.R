c6a19306329ae05d91985828b96fad12366376e9
dimitris.kontos <95762385+dimitriskontosClinBAY@users.noreply.github.com>
2022-02-08 08:04:39 +0200

    changes due to  GitHub SuperLinter findings
    pull request reviewer's suggestions

--- examples/Rules-class-IncrementsNumDoseLevels.R
+++ examples/Rules-class-IncrementsNumDoseLevels.R
@@ -1,10 +1,9 @@
 
 # In this example we define a rule for dose increments which would allow:
-# Maximum skip one dose level, that is 2 dose levels higher than the last dose 
+# Maximum skip one dose level, that is 2 dose levels higher than the last dose
 # given.
 # Maximum increment is explicitly defined as:
-myIncrements <- IncrementsNumDoseLevels(maxLevels=2,basisLevel = "lastGiven")
+myIncrements <- IncrementsNumDoseLevels(maxLevels = 2, basisLevel = "lastGiven")
 # Since the default method is based on the last dose given,
-# maximum increment can also be defined as: 
-myIncrements <- IncrementsNumDoseLevels(maxLevels=2)
-
+# maximum increment can also be defined as:
+myIncrements <- IncrementsNumDoseLevels(maxLevels = 2)
