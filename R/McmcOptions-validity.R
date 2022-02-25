a5ac6d6a862d5052e3cabae32ef8c89c2f0cc9d3
Wojtek <wojciech.wojciak@gmail.com>
2022-02-25 16:34:28 +0100

    Closes #136
    Closes #136

--- R/McmcOptions-validity.R
+++ R/McmcOptions-validity.R
@@ -17,7 +17,7 @@ NULL
 #'   object contains valid integer scalars `iterations`, `burnin` and `step`
 #'   as well as proper parameters for Random Number Generator.
 v_mcmc_options <- function(object) {
-  o <- Validate()
+  v <- Validate()
   allowed_rng_kinds <- c(
     "base::Wichmann-Hill",
     "base::Marsaglia-Multicarry",
@@ -26,29 +26,29 @@ v_mcmc_options <- function(object) {
     NA_character_
   )
 
-  o$check(
+  v$check(
     test_int(object@iterations, lower = 1L),
     "iterations must be integer scalar greater than or equal to 1"
   )
-  o$check(
+  v$check(
     test_int(object@burnin, lower = 0L),
     "burn-in must be non-negative integer scalar"
   )
   # This below check should not be conducted in above test, using
   # `upper = object@iterations -1` argument, since object@iterations might be
   # not-valid. In such a case `test_int` throws an internal error.
-  o$check(
+  v$check(
     test_true(object@burnin < object@iterations),
     "burn-in must be lower than iterations"
   )
-  o$check(
+  v$check(
     test_int(object@step, lower = 1L),
     "step must be integer scalar greater than or equal to 1"
   )
 
   is_rng_kind_scalar <- test_string(object@rng_kind, na.ok = TRUE)
-  o$check(is_rng_kind_scalar, "rng_kind must be a single string")
-  o$check(
+  v$check(is_rng_kind_scalar, "rng_kind must be a single string")
+  v$check(
     test_subset(object@rng_kind, allowed_rng_kinds),
     paste0(
       "rng_kind must one of the following: ",
@@ -58,16 +58,16 @@ v_mcmc_options <- function(object) {
   )
 
   is_rng_seed_scalar <- test_int(object@rng_seed, na.ok = TRUE)
-  o$check(is_rng_seed_scalar, "rng_seed must be an integer scalar")
+  v$check(is_rng_seed_scalar, "rng_seed must be an integer scalar")
 
   # Below `if` condition is not only reasonable but also needed as R CMD check
   # is activating some stricter checks and it fails when arguments to `||` are
   # not scalars, even if `||` works well with vectors of length > 1.
   if (is_rng_kind_scalar && is_rng_seed_scalar) {
-    o$check(
+    v$check(
       !is.na(object@rng_kind) || is.na(object@rng_seed),
       "rng_seed supplied but rng_kind not set"
     )
   }
-  o$result()
+  v$result()
 }
