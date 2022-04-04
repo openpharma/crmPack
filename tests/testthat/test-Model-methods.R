6212ff908381af8901b691ddbfb70191737b3aa5
Wojciech Wójciak <11532997+wwojciech@users.noreply.github.com>
2022-04-04 16:09:33 +0200

    Closes #171 (#172)
    Closes #171

--- tests/testthat/test-Model-methods.R
+++ tests/testthat/test-Model-methods.R
@@ -139,11 +139,7 @@ test_that("dose-LogisticNormal works as expected", {
   samples <- h_as_samples(list(alpha0 = c(0, -1, 1, 2), alpha1 = c(0, 2, 1, -1)))
 
   result <- dose(0.4, model, samples)
-  expect_equal(
-    result,
-    c(0.00000, 67.30876, 12.26265, 554.17921),
-    tolerance = 1e-05
-  )
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticNormal works as expected for scalar samples", {
@@ -151,7 +147,7 @@ test_that("dose-LogisticNormal works as expected for scalar samples", {
   samples <- h_as_samples(list(alpha0 = 5, alpha1 = 10))
 
   result <- dose(c(0.3, 0.6), model, samples)
-  expect_equal(result, c(27.86282, 31.58144), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticNormal throws the error when prob is not a valid scalar", {
@@ -178,11 +174,7 @@ test_that("dose-LogisticLogNormal works as expected", {
   samples <- h_as_samples(list(alpha0 = c(0, -1, 1, 2), alpha1 = c(0, 2, 1, -1)))
 
   result <- dose(0.4, model, samples)
-  expect_equal(
-    result,
-    c(0.00000, 67.30876, 12.26265, 554.17921),
-    tolerance = 1e-05
-  )
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticLogNormal works as expected for scalar samples", {
@@ -190,7 +182,7 @@ test_that("dose-LogisticLogNormal works as expected for scalar samples", {
   samples <- h_as_samples(list(alpha0 = 5, alpha1 = 10))
 
   result <- dose(c(0.3, 0.7), model, samples)
-  expect_equal(result, c(27.86282, 33.00809), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticLogNormal throws the error when prob is not a valid scalar", {
@@ -218,11 +210,7 @@ test_that("dose-LogisticLogNormalSub works as expected", {
   samples <- h_as_samples(list(alpha0 = c(0, -1, 1, 2), alpha1 = c(0, 2, 1, -1)))
 
   result <- dose(0.4, model, samples)
-  expect_equal(
-    result,
-    c(-Inf, 2.2972674, 0.5945349, 4.4054651),
-    tolerance = 1e-05
-  )
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticLogNormalSub works as expected for scalar samples", {
@@ -230,7 +218,7 @@ test_that("dose-LogisticLogNormalSub works as expected for scalar samples", {
   samples <- h_as_samples(list(alpha0 = 5, alpha1 = 10))
 
   result <- dose(c(0.3, 0.7), model, samples)
-  expect_equal(result, c(1.41527, 1.58473), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticLogNormalSub throws the error when prob is not a valid scalar", {
@@ -258,11 +246,7 @@ test_that("dose-ProbitLogNormal works as expected", {
   samples <- h_as_samples(list(alpha0 = c(0, -1, 1, 2), alpha1 = c(0, 2, 1, -1)))
 
   result <- dose(0.4, model, samples)
-  expect_equal(
-    result,
-    c(0, 10.458421, 2.055942, 68.540727),
-    tolerance = 1e-05
-  )
+  expect_snapshot(result)
 })
 
 test_that("dose-ProbitLogNormal works as expected for scalar samples", {
@@ -270,7 +254,7 @@ test_that("dose-ProbitLogNormal works as expected for scalar samples", {
   samples <- h_as_samples(list(alpha0 = 5, alpha1 = 10))
 
   result <- dose(c(0.3, 0.7), model, samples)
-  expect_equal(result, c(4.143915, 4.602138), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("dose-ProbitLogNormal throws the error when prob is not a valid scalar", {
@@ -298,11 +282,7 @@ test_that("dose-ProbitLogNormalRel works as expected", {
   samples <- h_as_samples(list(alpha0 = c(0, -1, 1, 2), alpha1 = c(0, 2, 1, -1)))
 
   result <- dose(0.4, model, samples)
-  expect_equal(
-    result,
-    c(-Inf, 0.7466529, -2.5066942, 4.5066942),
-    tolerance = 1e-05
-  )
+  expect_snapshot(result)
 })
 
 test_that("dose-ProbitLogNormalRel works as expected for scalar samples", {
@@ -310,7 +290,7 @@ test_that("dose-ProbitLogNormalRel works as expected for scalar samples", {
   samples <- h_as_samples(list(alpha0 = 5, alpha1 = 10))
 
   result <- dose(c(0.3, 0.7), model, samples)
-  expect_equal(result, c(-1.1048801, -0.8951199), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("dose-ProbitLogNormalRel throws the error when prob is not a valid scalar", {
@@ -338,11 +318,7 @@ test_that("dose-LogisticKadane works as expected", {
   samples <- h_as_samples(list(rho0 = c(0.1, 0.2, 0.3), gamma = c(10, 40, 80)))
 
   result <- dose(0.2, model, samples)
-  expect_equal(
-    result,
-    c(5.901396, 1, -305.087742),
-    tolerance = 1e-05
-  )
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticKadane works as expected for scalar samples", {
@@ -350,7 +326,7 @@ test_that("dose-LogisticKadane works as expected for scalar samples", {
   samples <- h_as_samples(list(rho0 = 0.15, gamma = 50))
 
   result <- dose(c(0.3, 0.7), model, samples)
-  expect_equal(result, c(43.3589, 124.2571), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticKadane throws the error when prob is not a valid scalar", {
@@ -376,16 +352,12 @@ test_that("dose-LogisticKadane throws the error when prob is not a valid scalar"
 
 test_that("dose-LogisticKadaneBetaGamma works as expected", {
   model <- h_get_logistic_kadane_beta_gam()
-  samples <- h_as_samples(list(rho0 = c(0.05, 0.1, 0.15),
-                               gamma = c(3, 7, 10)
-  )
+  samples <- h_as_samples(
+    list(rho0 = c(0.05, 0.1, 0.15), gamma = c(3, 7, 10))
   )
 
   result <- dose(0.2, model, samples)
-  expect_equal(
-    result,
-    c(2.228955,  4.205052, 3.925453), tolerance = 1e-05
-  )
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticKadaneBetaGamma works as expected for scalar samples", {
@@ -393,7 +365,7 @@ test_that("dose-LogisticKadaneBetaGamma works as expected for scalar samples", {
   samples <- h_as_samples(list(rho0 = 0.15, gamma = 50))
 
   result <- dose(c(0.3, 0.7), model, samples)
-  expect_equal(result, c(50.0000, 145.4914), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticKadaneBetaGamma throws the error when prob is not a valid scalar", {
@@ -421,11 +393,7 @@ test_that("dose-LogisticNormalMixture works as expected", {
   samples <- h_as_samples(list(alpha0 = c(0, -1, 1, 2), alpha1 = c(0, 2, 1, -1)))
 
   result <- dose(0.2, model, samples)
-  expect_equal(
-    result,
-    c(0, 1.6487213, 0.1839397, 59.1124488),
-    tolerance = 1e-05
-  )
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticNormalMixture works as expected for scalar samples", {
@@ -433,7 +401,7 @@ test_that("dose-LogisticNormalMixture works as expected for scalar samples", {
   samples <- h_as_samples(list(alpha0 = 5, alpha1 = 10))
 
   result <- dose(c(0.3, 0.7), model, samples)
-  expect_equal(result, c(1.114513, 1.320324), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticNormalMixture throws the error when prob is not a valid scalar", {
@@ -461,11 +429,7 @@ test_that("dose-LogisticNormalFixedMixture works as expected", {
   samples <- h_as_samples(list(alpha0 = c(0, -1, 1, 2), alpha1 = c(0, 2, 1, -1)))
 
   result <- dose(0.2, model, samples)
-  expect_equal(
-    result,
-    c(0, 41.218032, 4.598493, 1477.811220),
-    tolerance = 1e-05
-  )
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticNormalFixedMixture works as expected for scalar samples", {
@@ -473,7 +437,7 @@ test_that("dose-LogisticNormalFixedMixture works as expected for scalar samples"
   samples <- h_as_samples(list(alpha0 = 5, alpha1 = 10))
 
   result <- dose(c(0.3, 0.7), model, samples)
-  expect_equal(result, c(27.86282, 33.00809), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticNormalFixedMixture throws the error when prob is not a valid scalar", {
@@ -562,8 +526,7 @@ test_that("dose-LogisticIndepBeta works as expected", {
   )
 
   result <- dose(prob = 0.45, model = dlt_model, samples = samples)
-  expected <- c(0.4075, 0.4515, 0, 0.2998, 0.3321)
-  expect_equal(result, expected, tolerance = 0.0001)
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticIndepBeta works as expected for scalar samples", {
@@ -571,7 +534,7 @@ test_that("dose-LogisticIndepBeta works as expected for scalar samples", {
   samples <- h_as_samples(list(phi1 = -1, phi2 = 1))
 
   result <- dose(prob = c(0.45, 0.7), model = dlt_model, samples = samples)
-  expect_equal(result, c(2.224049, 6.342658), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticIndepBeta throws the error when prob is not a valid scalar", {
@@ -598,7 +561,7 @@ test_that("dose-LogisticIndepBeta-noSamples works as expected", {
   dlt_model <- h_get_logistic_indep_beta()
 
   result <- dose(prob = c(0.45, 0.55), model = dlt_model)
-  expect_equal(result, expected = c(188.1673, 289.2156), tolerance = 0.0001)
+  expect_snapshot(result)
 })
 
 test_that("dose-LogisticIndepBeta-noSamples throws the error when prob is not a valid scalar", {
@@ -623,11 +586,7 @@ test_that("prob-LogisticNormal works as expected", {
   samples <- h_as_samples(list(alpha0 = c(0, -1, 1, 2), alpha1 = c(0, 2, 1, -1)))
 
   result <- prob(60, model, samples)
-  expect_equal(
-    result,
-    c(0.5, 0.346297, 0.765365, 0.860287),
-    tolerance = 1e-06
-  )
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticNormal works as expected for scalar samples", {
@@ -635,7 +594,7 @@ test_that("prob-LogisticNormal works as expected for scalar samples", {
   samples <- h_as_samples(list(alpha0 = 5, alpha1 = 10))
 
   result <- prob(c(20, 60), model, samples)
-  expect_equal(result, c(0.01532378, 0.99891297), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticNormal throws the error when dose is not a valid scalar", {
@@ -665,11 +624,7 @@ test_that("prob-LogisticLogNormal works as expected", {
   )
 
   result <- prob(60, model, samples)
-  expect_equal(
-    result,
-    c(0.5000000, 0.3462969, 0.7653650, 0.8602873),
-    tolerance = 1e-06
-  )
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticLogNormal works as expected for scalar samples", {
@@ -677,7 +632,7 @@ test_that("prob-LogisticLogNormal works as expected for scalar samples", {
   samples <- h_as_samples(list(alpha0 = 5, alpha1 = 10))
 
   result <- prob(c(20, 60), model, samples)
-  expect_equal(result, c(0.01532378, 0.99891297), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticLogNormal throws the error when dose is not a valid scalar", {
@@ -701,11 +656,7 @@ test_that("prob-LogisticLogNormalSub works as expected", {
   samples <- h_as_samples(list(alpha0 = c(0, -1, 1, 2), alpha1 = c(0, 2, 1, -1)))
 
   result <- prob(4, model, samples)
-  expect_equal(
-    result,
-    c(0.5, 0.9525741, 0.9525741, 0.5),
-    tolerance = 1e-06
-  )
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticLogNormalSub works as expected for scalar samples", {
@@ -713,7 +664,7 @@ test_that("prob-LogisticLogNormalSub works as expected for scalar samples", {
   samples <- h_as_samples(list(alpha0 = 2, alpha1 = 0.5))
 
   result <- prob(c(4, 10), model, samples)
-  expect_equal(result, c(0.9525741, 0.9975274), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticLogNormalSub throws the error when dose is not a valid scalar", {
@@ -737,11 +688,7 @@ test_that("prob-ProbitLogNormal works as expected", {
   samples <- h_as_samples(list(alpha0 = c(0, -1, 1, 2), alpha1 = c(0, 2, 1, -1)))
 
   result <- prob(4, model, samples)
-  expect_equal(
-    result,
-    c(0.5, 0.01479359, 0.65990847, 0.99517026),
-    tolerance = 1e-06
-  )
+  expect_snapshot(result)
 })
 
 test_that("prob-ProbitLogNormal works as expected for scalar samples", {
@@ -749,7 +696,7 @@ test_that("prob-ProbitLogNormal works as expected for scalar samples", {
   samples <- h_as_samples(list(alpha0 = 2, alpha1 = 0.5))
 
   result <- prob(c(4, 10), model, samples)
-  expect_equal(result, c(0.9560059, 0.9847775), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("prob-ProbitLogNormal throws the error when dose is not a valid scalar", {
@@ -773,11 +720,7 @@ test_that("prob-ProbitLogNormalRel works as expected", {
   samples <- h_as_samples(list(alpha0 = c(0, -1, 1, 2), alpha1 = c(0, 2, 1, -1)))
 
   result <- prob(4, model, samples)
-  expect_equal(
-    result,
-    c(0.5, 0.9986501, 0.9986501, 0.5),
-    tolerance = 1e-06
-  )
+  expect_snapshot(result)
 })
 
 test_that("prob-ProbitLogNormalRel works as expected for scalar samples", {
@@ -785,7 +728,7 @@ test_that("prob-ProbitLogNormalRel works as expected for scalar samples", {
   samples <- h_as_samples(list(alpha0 = 2, alpha1 = 0.5))
 
   result <- prob(c(2, 2.5), model, samples)
-  expect_equal(result, c(0.9937903, 0.9956676), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("prob-ProbitLogNormalRel throws the error when dose is not a valid scalar", {
@@ -809,11 +752,7 @@ test_that("prob-LogisticKadane works as expected", {
   samples <- h_as_samples(list(rho0 = c(0.1, 0.2, 0.3), gamma = c(10, 40, 80)))
 
   result <- prob(4, model, samples)
-  expect_equal(
-    result,
-    c(0.1543506, 0.2084767, 0.3011106),
-    tolerance = 1e-06
-  )
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticKadane works as expected for scalar samples", {
@@ -821,7 +760,7 @@ test_that("prob-LogisticKadane works as expected for scalar samples", {
   samples <- h_as_samples(list(rho0 = 0.15, gamma = 50))
 
   result <- prob(c(2, 2.5), model, samples)
-  expect_equal(result, c(0.1526904, 0.1540504), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticKadane throws the error when dose is not a valid scalar", {
@@ -845,11 +784,7 @@ test_that("prob-LogisticNormalMixture works as expected", {
   samples <- h_as_samples(list(alpha0 = c(0, -1, 1, 2), alpha1 = c(0, 2, 1, -1)))
 
   result <- prob(60, model, samples)
-  expect_equal(
-    result,
-    c(0.5, 0.9969888, 0.9878859, 0.1976262),
-    tolerance = 1e-06
-  )
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticNormalMixture works as expected for scalar samples", {
@@ -857,7 +792,7 @@ test_that("prob-LogisticNormalMixture works as expected for scalar samples", {
   samples <- h_as_samples(list(alpha0 = 5, alpha1 = 10))
 
   result <- prob(c(1, 1.5), model, samples)
-  expect_equal(result, c(0.1265878, 0.8931358), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticNormalMixture throws the error when dose is not a valid scalar", {
@@ -881,11 +816,7 @@ test_that("prob-LogisticNormalFixedMixture works as expected", {
   samples <- h_as_samples(list(alpha0 = c(0, -1, 1, 2), alpha1 = c(0, 2, 1, -1)))
 
   result <- prob(60, model, samples)
-  expect_equal(
-    result,
-    c(0.5, 0.3462969, 0.7653650, 0.8602873),
-    tolerance = 1e-06
-  )
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticNormalFixedMixture works as expected for scalar samples", {
@@ -893,7 +824,7 @@ test_that("prob-LogisticNormalFixedMixture works as expected for scalar samples"
   samples <- h_as_samples(list(alpha0 = 5, alpha1 = 10))
 
   result <- prob(c(30, 45), model, samples)
-  expect_equal(result, c(0.4729623, 0.9810421), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticNormalFixedMixture throws the error when dose is not a valid scalar", {
@@ -931,11 +862,7 @@ test_that("prob-LogisticLogNormalMixture works as expected", {
   )
 
   result <- prob(60, model, samples)
-  expect_equal(
-    result,
-    c(0.6748043, 0.6726061, 0.2901927, 0.2901927),
-    tolerance = 1e-06
-  )
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticLogNormalMixture works as expected for single samples", {
@@ -948,7 +875,7 @@ test_that("prob-LogisticLogNormalMixture works as expected for single samples", 
     )
   )
   result <- prob(c(1, 1.5), model, samples)
-  expect_equal(result, c(0.2474127, 0.2829247), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticLogNormalMixture throws the error when dose is not a valid scalar", {
@@ -1032,8 +959,7 @@ test_that("prob-LogisticIndepBeta works as expected", {
   )
 
   result <- prob(dose = 100, model = dlt_model, samples = samples)
-  expected <- c(1.7e-05, 0.004098, 0.5, 0.995902, 0.999983)
-  expect_equal(result, expected, tolerance = 0.000001)
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticIndepBeta works as expected for scalar samples", {
@@ -1041,7 +967,7 @@ test_that("prob-LogisticIndepBeta works as expected for scalar samples", {
   samples <- h_as_samples(list(phi1 = -1, phi2 = 1))
 
   result <- prob(dose = c(6, 15), model = dlt_model, samples = samples)
-  expect_equal(result, c(0.6882090, 0.8465832), tolerance = 1e-05)
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticIndepBeta throws the error when dose is not a valid scalar", {
@@ -1064,7 +990,7 @@ test_that("prob-LogisticIndepBeta-noSamples works as expected", {
   dlt_model <- h_get_logistic_indep_beta()
 
   result <- prob(dose = c(500, 1000), model = dlt_model)
-  expect_equal(result, expected = c(0.6708009, 0.7955970), tolerance = 0.000001)
+  expect_snapshot(result)
 })
 
 test_that("prob-LogisticIndepBeta-noSamples throws the error when dose is not a valid scalar", {
