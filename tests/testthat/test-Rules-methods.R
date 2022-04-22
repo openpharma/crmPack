97a189592175ac67c533171a48ee5d758ad852c8
phillip <pc494@cam.ac.uk>
2022-04-22 13:11:27 +0100

    Manual linter corrections

--- tests/testthat/test-Rules-methods.R
+++ tests/testthat/test-Rules-methods.R
@@ -337,10 +337,10 @@ test_that("NextBestInfTheory can be initiliazed ",{
 
 test_that("Helper function (h_info_theory_dist) for NextBestInfTheory is correct",{
   # values calculated using a different program
-  p = c(0.01,0.2,0.7)
-  gamma = c(0.5,0,0.3)
-  a = c(1,1.8,0.5)
-  expected <- c(2425.25,1.05,1.99)
-  calculated <- h_info_theory_dist(p,gamma,a)
-  expect_equal(calculated,expected,tolerance=0.01)
+  p <- c(0.01, 0.2, 0.7)
+  gamma <- c(0.5, 0, 0.3)
+  a <- c(1, 1.8, 0.5)
+  expected <- c(2425.25, 1.05, 1.99)
+  calculated <- h_info_theory_dist(p, gamma, a)
+  expect_equal(calculated, expected, tolerance=0.01)
 })
