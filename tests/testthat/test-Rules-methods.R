a01e2378cabcd611e673723aa91125f808f3c5f3
phillip <pc494@cam.ac.uk>
2022-04-27 17:44:50 +0100

    Reference implementation had an error, now fixed

--- tests/testthat/test-Rules-methods.R
+++ tests/testthat/test-Rules-methods.R
@@ -340,7 +340,7 @@ test_that("Helper function (h_info_theory_dist) for NextBestInfTheory is correct
   p <- c(0.01, 0.2, 0.7)
   gamma <- c(0.5, 0, 0.3)
   a <- c(1, 1.8, 0.5)
-  expected <- c(2425.25, 1.05, 1.99)
+  expected <- c(24.25, 0.76, 1.16)
   calculated <- h_info_theory_dist(p, gamma, a)
   expect_equal(calculated, expected, tolerance=0.01)
 })
