abcbae37a13692b36d78ab513217f104acee78c2
phillip <pc494@cam.ac.uk>
2022-04-28 10:52:19 +0100

    linter fixes after review

--- tests/testthat/test-Rules-methods.R
+++ tests/testthat/test-Rules-methods.R
@@ -332,7 +332,7 @@ test_that("StoppingLowestDoseHSRBeta works correctly if first active dose is not
 
 test_that("NextBestInfTheory can be initiliazed ", {
   # set up
-  newMyNextBest <- NextBestInfTheory(target = 0.25, asymmetry=0.1)
+  new_my_next_best <- NextBestInfTheory(target = 0.25, asymmetry = 0.1)
 })
 
 test_that("Helper function (h_info_theory_dist) for NextBestInfTheory is correct", {
@@ -342,5 +342,5 @@ test_that("Helper function (h_info_theory_dist) for NextBestInfTheory is correct
   a <- c(1, 1.8, 0.5)
   expected <- c(24.25, 0.76, 1.16)
   calculated <- h_info_theory_dist(p, gamma, a)
-  expect_equal(calculated, expected, tolerance=0.01)
+  expect_equal(calculated, expected, tolerance = 0.01)
 })
