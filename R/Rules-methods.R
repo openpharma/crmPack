584f4b27105afe6d0dcf53da700259515c32791e
phillip <pc494@cam.ac.uk>
2022-05-24 16:37:36 +0100

    Meeting updates, finishing PR 122

--- R/Rules-methods.R
+++ R/Rules-methods.R
@@ -3,6 +3,26 @@
 #' @include Rules-class.R
 NULL
 
+#' Calculating the Information Theoretic Distance
+#'
+#' @description `r lifecycle::badge("experimental")`
+#'
+#' Helper function which provides the value of the
+#' divergence as given by equation in (7) in the reference at
+#' https://doi.org/10.1002/sim.8450.
+#'
+#' @param prob (`numeric`)\cr probability of a DLT occurring.
+#' @param target (`numeric `)\cr target probability of a DLT.
+#' @param asymmetry (`number`)\cr describes the rate of penalization
+#'   for overly toxic does, range 0 to 2.
+#'
+#' @export
+#' @examples
+#' h_info_theory_dist(c(0.5, 0.2), c(0.5, 0.1), 1.2)
+h_info_theory_dist <- function(prob, target, asymmetry) {
+  ((prob - target)^2) / (((prob^asymmetry) * (1 - prob)^(2 - asymmetry)))
+}
+
 # nolint start
 
 ##' Find the next best dose
@@ -659,17 +679,6 @@ setMethod("nextBest",
             bestDose <- doses[bestIndex]
             return(list(value = bestDose))
           })
-
-#' @describeIn nextBest Helper function which provides the value of the
-#'   divergence as given by equation in (7) in the reference at
-#'   https://doi.org/10.1002/sim.8450.
-#' @param prob of a DLT occuring
-#' @param target probablity of a DLT
-#' @param asymmetry describes the rate of penalisation for overly toxic does, range 0 to 2
-#' @export
-h_info_theory_dist <- function(prob, target, asymmetry) {
-  ((prob - target)^2) / (((prob^asymmetry) * (1 - prob)^(2 - asymmetry)))
-}
 
 ##' @describeIn nextBest Method for `NextBestInfTheory` class, which will give
 ##'   the appropriate dose within an information theoretic framework
