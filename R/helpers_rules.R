df7a8dca7087f10c2dd1f3965e80ebcad1dfc331
Wojciech <11532997+wwojciech@users.noreply.github.com>
2022-08-08 15:10:40 +0200

    Closes #294 (#295)
    * Closes #294
    
    Closes #294
    
    * Closes #294 (help typo)
    
    Closes #294 (help typo)

--- R/helpers_rules.R
+++ R/helpers_rules.R
@@ -27,15 +27,21 @@ h_info_theory_dist <- function(prob, target, asymmetry) {
   ((prob - target)^2) / (((prob^asymmetry) * (1 - prob)^(2 - asymmetry)))
 }
 
-#' Find a Matrix for Variance According to Yeung et. al (2015)
+#' Credibility Intervals for Max Gain and Target Doses at `nextBest-NextBestMaxGain` Method.
 #'
 #' @description `r lifecycle::badge("experimental")`
 #'
-#' Helper function which computes the matrix required for calculation of the
-#' variance of the logarithm of the maximum gain dose, based on Yeung et. al (2015)
-#' paper. This helper function is used by [`nextBest-NextBestMaxGain()`] method.
+#' Helper function for [`nextBest-NextBestMaxGain()`] method. It computes a
+#' 95% credibility intervals for given target dose and max gain dose.
+#' It also returns a ratio of upper and lower bounds of the interval.
 #'
-#' @param log_dose_mg (`number`)\cr the log of dose corresponding to the maximum gain.
+#' @param dose_target (`number`)\cr target dose estimate.
+#' @param dose_mg (`number`)\cr the dose corresponding to the maximum gain.
+#' @param prob_target (`proportion`)\cr target DLT probability.
+#' @param placebo (`flag`)\cr if `TRUE` the first dose level in the dose grid used
+#'   is considered as placebo. This is needed to adjust the max gain dose using
+#'   efficacy constant value. If the `placebo` was used, then the `model_eff@const`
+#'   is added to `dose_mg`.
 #' @param model (`ModelTox`)\cr the DLT model.
 #' @param model_eff (`Effloglog`)\cr the efficacy model.
 #'
@@ -47,18 +53,61 @@ h_info_theory_dist <- function(prob, target, asymmetry) {
 #'
 #' @export
 #'
-h_delta_g_yeung <- function(log_dose_mg, model, model_eff) {
-  assert_number(log_dose_mg, na.ok = TRUE)
+h_next_best_mg_ci <- function(dose_target,
+                              dose_mg,
+                              prob_target,
+                              placebo,
+                              model,
+                              model_eff) {
+  assert_number(dose_target, na.ok = TRUE)
+  assert_number(dose_mg, na.ok = TRUE)
+  assert_probability(prob_target)
+  assert_flag(placebo)
   assert_class(model, "ModelTox")
   assert_class(model_eff, "Effloglog")
 
+  # Find the variance of the log of target dose.
+  mat <- matrix(
+    c(
+      -1 / (model@phi2),
+      -(log(prob_target / (1 - prob_target)) - model@phi1) / (model@phi2)^2
+    ),
+    nrow = 1
+  )
+  var_dose_target <- as.vector(mat %*% model@Pcov %*% t(mat))
+
+  # 95% credibility interval for target dose.
+  ci_td <- exp(log(dose_target) + c(-1, 1) * 1.96 * sqrt(var_dose_target))
+  ci_ratio_td <- ci_td[2] / ci_td[1]
+
+  # Find the variance of the log of dose_mg.
+  # First, find the covariance matrix of all the parameters, phi1, phi2, theta1 and theta2
+  # given that phi1 and phi2 are independent of theta1 and theta2.
+  log_dose_mg <- log(dose_mg + ifelse(placebo, model_eff@const, 0))
+
+  # Find a delta_g matrix for a variance according to Yeung et. al (2015).
   mean_eff_mg <- model_eff@theta1 + model_eff@theta2 * log(log_dose_mg)
   denom <- model@phi2 * mean_eff_mg * (1 + model@phi2 * log_dose_mg)
   dgphi1 <- -(mean_eff_mg * log_dose_mg * model@phi2 - model_eff@theta2) / denom
   dgphi2 <- -(log_dose_mg * (mean_eff_mg * (1 + log_dose_mg * model@phi2) - model_eff@theta2)) / denom
   dgtheta1 <- -(log_dose_mg * model@phi2) / denom
   dgtheta2 <- -(exp(model@phi1 + model@phi2 * log_dose_mg) * (model@phi2 * log_dose_mg * log(log_dose_mg) - 1) - 1) / denom
-  matrix(c(dgphi1, dgphi2, dgtheta1, dgtheta2), 4, 1)
+  delta_g <- matrix(c(dgphi1, dgphi2, dgtheta1, dgtheta2), 4, 1)
+
+  zero_matrix <- matrix(0, 2, 2)
+  cov_beta <- cbind(rbind(model@Pcov, zero_matrix), rbind(zero_matrix, model_eff@Pcov))
+  var_log_dose_mg <- as.vector(t(delta_g) %*% cov_beta %*% delta_g)
+
+  # 95% credibility interval for max gain dose.
+  ci_mg <- exp(log_dose_mg + c(-1, 1) * 1.96 * sqrt(var_log_dose_mg))
+  ci_ratio_mg <- ci_mg[2] / ci_mg[1]
+
+  list(
+    ci_dose_target = ci_td,
+    ci_ratio_dose_target = ci_ratio_td,
+    ci_dose_mg = ci_mg,
+    ci_ratio_dose_mg = ci_ratio_mg
+  )
 }
 
 ## next best at grid ----
@@ -69,47 +118,24 @@ h_delta_g_yeung <- function(log_dose_mg, model, model_eff) {
 #'
 #' Helper function that for a given target doses finds the dose in grid that is
 #' closest and below the target. There are four different targets in the context
-#' of [`nextBest-NextBestMaxGain()`] method: min(`dose_mg`, `dose_target_drt`),
+#' of [`nextBest-NextBestMaxGain()`] method: \eqn{min(`dose_mg`, `dose_target_drt`)},
 #' `dose_mg`, `dose_target_drt` or `dose_target_eot`.
-#'
-#' @note For placebo design, if safety allows, exclude placebo from the
-#'   recommended next doses.
 #'
 #' @param dose_target_drt (`number`)\cr target dose estimate during the trial.
 #' @param dose_target_eot (`number`)\cr target dose estimate at the end of the trial.
 #' @param dose_mg (`number`)\cr the dose corresponding to the maximum gain.
-#' @param prob_target_eot (`proportion`)\cr target DLT probability at the end of the trial.
 #' @param doses_eligible (`numeric`)\cr eligible doses from the grid.
-#' @param placebo (`flag`)\cr if `TRUE` the first dose level in the dose grid used
-#'   is considered as placebo. This is needed to adjust the max gain dose using
-#'   efficacy constant value. If the `placebo` was used, then the `model_eff@const`
-#'   is added to `dose_mg`.
-#' @param model (`ModelTox`)\cr the DLT model.
-#' @param model_eff (`Effloglog`)\cr the efficacy model.
-#' @param compute_ci (`flag`)\cr if `TRUE` the 95% credibility intervals (and
-#'   corresponding ratios) are computed for target dose (end of the trial) and
-#'   the maximum gain dose.
 #'
 #' @export
 #'
 h_next_best_mg_doses_at_grid <- function(dose_target_drt,
                                          dose_target_eot,
                                          dose_mg,
-                                         prob_target_eot,
-                                         doses_eligible,
-                                         placebo,
-                                         model,
-                                         model_eff,
-                                         compute_ci = TRUE) {
+                                         doses_eligible) {
   assert_number(dose_target_drt, na.ok = TRUE)
   assert_number(dose_target_eot, na.ok = TRUE)
   assert_number(dose_mg, na.ok = TRUE)
-  assert_probability(prob_target_eot)
   assert_numeric(doses_eligible, finite = TRUE, any.missing = FALSE)
-  assert_flag(placebo)
-  assert_class(model, "ModelTox")
-  assert_true(test_class(model_eff, "Effloglog") || test_class(model_eff, "EffFlexi"))
-  assert_flag(compute_ci)
 
   # h_find_interval assumes that elements in doses_eligible are strictly increasing.
   next_dose_lev <- h_find_interval(min(dose_mg, dose_target_drt), doses_eligible)
@@ -130,46 +156,6 @@ h_next_best_mg_doses_at_grid <- function(dose_target_drt,
     next_dose_eot = next_dose_eot,
     next_dose_mg = next_dose_mg
   )
-
-  if (compute_ci) {
-    # Find the variance of the log of dose_mg.
-    # First, find the covariance matrix of all the parameters, phi1, phi2, theta1 and theta2
-    # given that phi1 and phi2 are independent of theta1 and theta2.
-    log_dose_mg <- log(dose_mg + ifelse(placebo, model_eff@const, 0))
-    delta_g <- h_delta_g_yeung(log_dose_mg, model, model_eff)
-    zero_matrix <- matrix(0, 2, 2)
-    cov_beta <- cbind(rbind(model@Pcov, zero_matrix), rbind(zero_matrix, model_eff@Pcov))
-    var_log_dose_mg <- as.vector(t(delta_g) %*% cov_beta %*% delta_g)
-
-    # 95% credibility interval.
-    ci_dose_mg <- exp(log_dose_mg + c(-1, 1) * 1.96 * sqrt(var_log_dose_mg))
-    ci_ratio_dose_mg <- as.numeric(ci_dose_mg[2] / ci_dose_mg[1])
-
-    # Find the variance of the log of the dose_target_eot.
-    mat <- matrix(
-      c(
-        -1 / (model@phi2),
-        -(log(prob_target_eot / (1 - prob_target_eot)) - model@phi1) / (model@phi2)^2
-      ),
-      nrow = 1
-    )
-    var_dose_target_eot <- as.vector(mat %*% model@Pcov %*% t(mat))
-
-    # 95% credibility interval.
-    ci_td_eot <- exp(log(dose_target_eot) + c(-1, 1) * 1.96 * sqrt(var_dose_target_eot))
-    ci_ratio_td_eot <- ci_td_eot[2] / ci_td_eot[1]
-
-    next_dose_list <- c(
-      next_dose_list,
-      list(
-        ci_td_eot = ci_td_eot,
-        ci_ratio_td_eot = ci_ratio_td_eot,
-        ci_dose_mg = ci_dose_mg,
-        ci_ratio_dose_mg = ci_ratio_dose_mg
-      )
-    )
-  }
-  next_dose_list
 }
 
 ## plot ----
