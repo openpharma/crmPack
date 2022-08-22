2e06bea18635c050c5850c2933909b3cd795af91
dimitris.kontos <95762385+dimitriskontosClinBAY@users.noreply.github.com>
2022-08-22 16:33:51 +0300

    Added StopSpecificDose validation funtion

--- R/Rules-validity.R
+++ R/Rules-validity.R
@@ -242,3 +242,15 @@ v_stopping_mtd_cv <- function(object) {
   )
   v$result()
 }
+
+
+#' @describeIn v_stop_specific_dose validates that the [`StopSpecificDose`] object
+#'   contains valid dose assigned as input.
+v_stop_specific_dose <- function(object) {
+  v <- Validate()
+  v$check(
+    test_number(object@dose, finite = TRUE),
+    "dose needs to be a single finite number"
+  )
+  v$result()
+}
