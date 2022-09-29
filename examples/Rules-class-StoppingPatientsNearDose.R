8c342511b4135c0268acca094ea8e960d2635193
dimitris.kontos <95762385+dimitriskontosClinBAY@users.noreply.github.com>
2022-09-29 11:59:54 +0300

    updates due to linter error message

--- examples/Rules-class-StoppingPatientsNearDose.R
+++ examples/Rules-class-StoppingPatientsNearDose.R
@@ -1,7 +1,7 @@
 # As example, here is the rule for:
 #   stopping the study if at least 9 patients were dosed at a dose within (1 +/- 0.2)
 #   of the next best dose
-my_Stopping <- StoppingPatientsNearDose(
+my_stopping <- StoppingPatientsNearDose(
   nPatients = 9,
   percentage = 0.2
 )
