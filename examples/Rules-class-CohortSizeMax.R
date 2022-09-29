8359a326e87d5ca2787ea16bb786e2b14fb552a5
dimitris.kontos <95762385+dimitriskontosClinBAY@users.noreply.github.com>
2022-09-29 11:36:33 +0300

    fixed linter problems

--- examples/Rules-class-CohortSizeMax.R
+++ examples/Rules-class-CohortSizeMax.R
@@ -8,14 +8,14 @@ my_size_1 <- CohortSizeRange(
 
 # Rule for having cohort of size 1 until no DLT were observed
 #      and having cohort of size 3 as soon as 1 DLT is observed
-mySize2 <- CohortSizeDLT(
+my_size_2 <- CohortSizeDLT(
   DLTintervals = c(0, 1),
   cohortSize = c(1, 3)
 )
 
 # Create a list of cohort size rules of class 'CohortSizeMax' which will then be
 # combined with the 'max' operation
-mySize <- CohortSizeMax(cohortSizeList = list(
-  mySize1,
-  mySize2
+my_size <- CohortSizeMax(cohortSizeList = list(
+  my_size_1,
+  my_size_2
 ))
