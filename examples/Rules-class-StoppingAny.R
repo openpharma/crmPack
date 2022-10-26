30459b9d38e45ef55149dcf38443fc2e6bb3b6ec
Wojtek <wojciech.wojciak@gmail.com>
2022-10-26 15:05:23 +0200

    Closes #412 - PR Review
    Closes #412 - PR Review

--- examples/Rules-class-StoppingAny.R
+++ examples/Rules-class-StoppingAny.R
@@ -5,8 +5,8 @@ my_stopping2 <- StoppingTargetProb(target = c(0.2, 0.35), prob = 0.5)
 my_stopping3 <- StoppingMinPatients(nPatients = 20)
 
 # Create a list of stopping rules (of class `StoppingAny`) which would then be
-# summarized by the `any` function, meaning that the study would be stopped only
-# if any of the single stopping rules is `TRUE`.
+# summarized by the `any` function, meaning that the study would be stopped if
+# any of the single stopping rules is `TRUE`.
 my_stopping <- StoppingAny(
   stop_list = c(my_stopping1, my_stopping2, my_stopping3)
 )
