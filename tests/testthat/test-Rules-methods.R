6015fbd1e9fd0b2a0d365357ce7704c6f92c46a4
Robert <Adams>
2023-02-13 12:56:43 +0100

    fixed parameter naming for label-test_that functions

--- tests/testthat/test-Rules-methods.R
+++ tests/testthat/test-Rules-methods.R
@@ -1051,13 +1051,13 @@ test_that("Default label assignment for stopping rules works correctly.", {
   my_model <- h_get_logistic_kadane()
   my_samples <- mcmc(my_data, my_model, h_get_mcmc_options(samples = 1000, burnin = 1000))
 
-  stopping <- StoppingMinCohorts(nCohorts=3, report_label = "default")
+  my_stopping <- StoppingMinCohorts(nCohorts=3, report_label = "default")
 
-  result <- stopTrial(stopping=stopping,
-                      model = model,
+  result <- stopTrial(stopping = my_stopping,
+                      model = my_model,
                       samples = my_samples,
                       dose = 7,
-                      data=data)
+                      data = my_data)
 
   expect_equal(attr(result, "report_label"), "Minimum number of 3 cohorts reached")
 })
@@ -1069,13 +1069,13 @@ test_that("Customized label assignment for stopping rules works correctly.", {
   my_model <- h_get_logistic_kadane()
   my_samples <- mcmc(my_data, my_model, h_get_mcmc_options(samples = 1000, burnin = 1000))
 
-  stopping <- StoppingMinCohorts(nCohorts=3, report_label = "a customized label")
+  my_stopping <- StoppingMinCohorts(nCohorts=3, report_label = "a customized label")
 
-  result <- stopTrial(stopping=stopping,
-                      model = model,
+  result <- stopTrial(stopping = my_stopping,
+                      model = my_model,
                       samples = my_samples,
                       dose = 7,
-                      data=data)
+                      data=my_data)
 
   expect_equal(attr(result, "report_label"), "a customized label")
 })
