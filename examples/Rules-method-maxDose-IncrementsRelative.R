2039905f095fe6bbc43878dc4170d2f00ea9b86f
Wojtek <wojciech.wojciak@gmail.com>
2023-03-21 15:46:29 +0100

    Closes #496
    Closes #496

--- examples/Rules-method-maxDose-IncrementsRelative.R
+++ examples/Rules-method-maxDose-IncrementsRelative.R
@@ -1,20 +1,23 @@
 
-# Create the data
-data <- Data(x=c(0.1, 0.5, 1.5, 3, 6, 8, 8, 8),
-             y=c(0, 0, 0, 0, 0, 0, 1, 0),
-             cohort=c(0, 1, 2, 3, 4, 5, 5, 5),
-             doseGrid=
-               c(0.1, 0.5, 1.5, 3, 6, 8,
-                 seq(from=10, to=80, by=2)))
+# Example of usage for `IncrementsRelative` maxDose class.
 
+# Create the data.
+my_data <- Data(
+  x = c(0.1, 0.5, 1.5, 3, 6, 8, 8, 8),
+  y = c(0, 0, 0, 0, 0, 0, 1, 0),
+  ID = 1:8,
+  cohort = c(0, 1, 2, 3, 4, 5, 5, 5),
+  doseGrid = c(0.1, 0.5, 1.5, 3, 6, 8, seq(from = 10, to = 80, by = 2))
+)
 
-# In this example we define a rule for dose increments which would allow:
-#   - doubling the dose if the last dose was below 20
-#   - only increasing the dose by 1.33 if the last dose was equal or above 20
-myIncrements <- IncrementsRelative(intervals=c(0, 20),
-                                   increments=c(1, 0.33))
+# Define a rule for dose increments which allows:
+#  - doubling the dose if the last dose was below 20,
+#  - increasing the dose by 33% of the last dose, only if the last dose was
+#    above or equal to 20.
+my_increments <- IncrementsRelative(
+  intervals = c(0, 20),
+  increments = c(1, 0.33)
+)
 
-# Based on the rule above, we then calculate the maximum dose allowed
-nextMaxDose <- maxDose(myIncrements,
-                       data=data)
-
+# Based on the rule above, the maximum dose allowed is:
+my_next_max_dose <- maxDose(my_increments, data = my_data)
