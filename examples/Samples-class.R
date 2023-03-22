00631df8216ba34498cb02983876f4194627f3cf
Wojtek <wojciech.wojciak@gmail.com>
2023-03-22 21:34:12 +0100

    Closes #503 - minor example update
    Closes #503 - minor example update

--- examples/Samples-class.R
+++ examples/Samples-class.R
@@ -1,5 +1,4 @@
-
-# The mcmc options that were used to generate the samples.
+# The MCMC options that were used to generate the samples.
 my_options <- McmcOptions(
   burnin = 1000,
   step = 2,
@@ -7,8 +6,8 @@ my_options <- McmcOptions(
 )
 
 # Create an object of class 'Samples'
-# Here the parameters 'alpha' and 'beta' are randomly generated. Of course in
-# a real example these would come as an output from mcmc procedures
+# Here the parameters 'alpha' and 'beta' are randomly generated. Of course, in
+# a real example these would be a samples coming from MCMC procedures.
 my_samples <- Samples(
   data = list(alpha = rnorm(1000), beta = rnorm(1000)),
   options = my_options
