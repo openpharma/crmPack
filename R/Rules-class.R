185f4578e77b265a4974eff5dedf7900b5d98779
Oliver Boix <95433070+0liver0815@users.noreply.github.com>
2023-03-29 17:35:53 +0200

    Add attional text how placebo is handled

--- R/Rules-class.R
+++ R/Rules-class.R
@@ -733,8 +733,9 @@ NextBestProbMTDLTE <- function(target) {
 #' for each dose per iteration and select the dose that has the smallest toxicity
 #' probability distance to the target. The dose with the highest frequency
 #' of being selected as MTD across iterations is the next best dose. Placebo
-#' is not considered in the calculation and removed from the dose grid for
-#' any calculations.
+#' is not considered as the next dose and for that reason not used in
+#' calculations. I.e. for placebo the toxicity probability distance to target
+#' is not calculated and taken into account for determination of the next dose.
 #'
 #' @slot target (`numeric`)\cr the target toxicity probability.
 #'
