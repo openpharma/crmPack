5de5d15f319fd7bb680c778e02f3effabc3b7dc3
Robert <Adams>
2023-03-30 15:08:07 +0200

    Corrected a test glitch

--- tests/testthat/test-Design-methods.R
+++ tests/testthat/test-Design-methods.R
@@ -93,33 +93,6 @@ test_that("NextBestInfTheory produces consistent results with a dataset", {
   expect_snapshot(result@meanFit)
 })
 
-
-
-test_that("reporting labels and logicals are correctly returned using the recursive function in simulate", {
-  my_data <- h_get_data(placebo = FALSE)
-  model <- LogisticLogNormal(
-    mean = c(-0.85, 1),
-    cov =
-      matrix(c(1, -0.5, -0.5, 1),
-        nrow = 2
-      ),
-    ref_dose = 56
-  )
-
-
-  increments <- IncrementsRelative(
-    intervals = c(0, 20),
-    increments = c(1, 0.33)
-  )
-  new_my_next_best <- NextBestNCRM(
-    target = c(0.2, 0.35),
-    overdose = c(0.35, 1),
-    max_overdose_prob = 0.25
-  )
-  my_size <- CohortSizeConst(size = 3)
-})
-
-
 test_that("reporting labels and logicals are correctly returned using the recursive function in simulate", {
   my_data <- h_get_data(placebo = FALSE)
   model <- LogisticLogNormal(
