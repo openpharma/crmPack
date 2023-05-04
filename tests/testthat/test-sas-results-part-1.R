0ac12f10bd26d066f6731dc6a9d8a2bd7bef5055
Marlene <marlene.schulte-goebel@merckgroup.com>
2023-05-04 16:37:07 +0200

    Added skip_on_cran() to the tests

--- tests/testthat/test-sas-results-part-1.R
+++ tests/testthat/test-sas-results-part-1.R
@@ -1,6 +1,9 @@
 test_that("Posterior summaries for probabilities of
           DLT (2-parameter logistic model) and recommended doses (NCRMLoss):
           crmPack vs. SAS - Example 1", {
+
+  skip_on_cran()
+
   set.seed(0504201914)
   mcmc_options <- McmcOptions(
     burnin = 5000,
