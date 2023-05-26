f70c905cbeca21755276430f9ad920e5279cfb92
Guanya Peng <guanya.peng24@gmail.com>
2023-05-26 14:23:53 +0200

    update DADesign examples based on feedback

--- examples/Design-class-DADesign.R
+++ examples/Design-class-DADesign.R
@@ -1,12 +1,8 @@
-# nolint start
-
-# Define the dose-grid and PEM parameters
 empty_data <- DataDA(doseGrid = c(
   0.1, 0.5, 1, 1.5, 3, 6,
   seq(from = 10, to = 80, by = 2)
 ), Tmax = 60)
 
-# Initialize the mDA-CRM model
 npiece_ <- 10
 Tmax_ <- 60
 
@@ -23,7 +19,6 @@ model <- DALogisticLogNormal(
   c_par = 2
 )
 
-# Choose the rule for dose increments
 my_increments <- IncrementsRelative(
   intervals = c(0, 20),
   increments = c(1, 0.33)
@@ -35,7 +30,6 @@ my_next_best <- NextBestNCRM(
   max_overdose_prob = 0.25
 )
 
-# Choose the rule for the cohort-size
 my_size1 <- CohortSizeRange(
   intervals = c(0, 30),
   cohort_size = c(1, 3)
@@ -48,7 +42,6 @@ my_size2 <- CohortSizeDLT(
 
 my_size <- maxSize(my_size1, my_size2)
 
-# Choose the rule for stopping
 my_stopping1 <- StoppingTargetProb(
   target = c(0.2, 0.35),
   prob = 0.5
@@ -58,10 +51,8 @@ my_stopping2 <- StoppingMinPatients(nPatients = 50)
 
 my_stopping <- (my_stopping1 | my_stopping2)
 
-# Choose the safety window
 my_safety_window <- SafetyWindowConst(c(6, 2), 7, 7)
 
-# Initialize the design
 design <- DADesign(
   model = model,
   increments = my_increments,
