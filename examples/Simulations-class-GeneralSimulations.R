5a2b0ef4bb47fef2aea917781a560421c0530f7c
Guanya Peng <guanya.peng24@gmail.com>
2023-06-09 15:40:23 +0200

    fix example

--- examples/Simulations-class-GeneralSimulations.R
+++ examples/Simulations-class-GeneralSimulations.R
@@ -1,6 +1,6 @@
 data <- list(
-  Data(x = 1:3, y = 0:1, doseGrid = 1:3, ID = 1L:3L, cohort = 1L:3L),
-  Data(x = 4:6, y = 0:1, doseGrid = 4:6, ID = 1L:3L, cohort = 1L:3L)
+  Data(x = 1:3, y = c(0,1,0), doseGrid = 1:3, ID = 1L:3L, cohort = 1L:3L),
+  Data(x = 4:6, y = c(0,1,0), doseGrid = 4:6, ID = 1L:3L, cohort = 1L:3L)
 )
 
 doses <- c(1, 2)
