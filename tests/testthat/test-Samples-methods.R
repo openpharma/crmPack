4a90b488a9b4983739dcf1671f93d9c6d1cc75ac
John Kirkpatrick <133956382+Puzzled-Face@users.noreply.github.com>
2023-06-14 09:17:00 +0100

    Update tests/testthat/test-Samples-methods.R
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>

--- tests/testthat/test-Samples-methods.R
+++ tests/testthat/test-Samples-methods.R
@@ -104,7 +104,7 @@ test_that("approximate fails gracefully with bad input", {
   )
 })
 
-test_that("Approximate works correctly", {
+test_that("approximate works correctly", {
   data <- Data(
     x = c(0.1, 0.5, 1.5, 3, 6, 10, 10, 10),
     y = c(0, 0, 0, 0, 0, 0, 1, 0),
