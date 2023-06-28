8acbbbc08a5562bd8fedb4f90be61d562a1aadaa
Puzzled-Face <john.kirkpatrick@roche.com>
2023-06-28 11:58:43 +0000

    Silly typo in validation

--- R/helpers_jags.R
+++ R/helpers_jags.R
@@ -28,7 +28,7 @@ NULL
 #' @example examples/helpers-jags_add_dummy.R
 #'
 h_jags_add_dummy <- function(object, where, dummy = 0) {
-  assert_class(object, "DataOrdinal")
+  assert_true(class(object) %in% c("Data", "DataOrdinal"))
   assert_character(where)
   assert_subset(where, slotNames(object))
   assert_number(dummy)
