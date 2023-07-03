53ac104a27c3482192e13d529c99fbd862bf0b39
Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
2023-07-03 10:48:34 +0200

    clean new branch (#641)

--- tests/testthat/test-ModelParams-class.R
+++ tests/testthat/test-ModelParams-class.R
@@ -24,3 +24,13 @@ test_that("ModelParamsNormal object can be created with user constructor", {
   )
   expect_valid(result, "ModelParamsNormal")
 })
+
+test_that("ModelParamsNormal object can be created also for larger dimensions", {
+  result <- expect_silent(
+    ModelParamsNormal(
+      mean = c(1, 5, 4, 6),
+      cov = diag(3, ncol = 4, nrow = 4)
+    )
+  )
+  expect_valid(result, "ModelParamsNormal")
+})
