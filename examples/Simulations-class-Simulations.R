dce13c544f43c1b6e0c3e512d5145c6fb58865e9
Guanya Peng <guanya.peng24@gmail.com>
2023-07-03 10:03:16 +0200

    update example

--- examples/Simulations-class-Simulations.R
+++ examples/Simulations-class-Simulations.R
@@ -1,3 +1,24 @@
+data <- list(
+  Data(
+    x = 1:2,
+    y = 0:1,
+    doseGrid = 1:2,
+    ID = 1L:2L,
+    cohort = 1L:2L
+  ),
+  Data(
+    x = 3:4,
+    y = 0:1,
+    doseGrid = 3:4,
+    ID = 1L:2L,
+    cohort = 1L:2L
+  )
+)
+
+doses <- c(1, 2)
+
+seed <- as.integer(123)
+
 fit <- list(
   c(0.1, 0.2),
   c(0.3, 0.4)
@@ -5,4 +26,4 @@ fit <- list(
 
 stop_reasons <- list("A", "B")
 
-simulations <- Simulations(fit = fit, stop_reasons = stop_reasons)
+simulations <- Simulations(fit = fit, stop_reasons = stop_reasons, data, doses, seed)
