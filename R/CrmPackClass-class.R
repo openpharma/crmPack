84923d89387df782e74c223e80c8f180440eb121
Puzzled-Face <john.kirkpatrick@roche.com>
2023-07-26 08:28:42 +0000

    Fixing pkgdown

--- R/CrmPackClass-class.R
+++ R/CrmPackClass-class.R
@@ -4,9 +4,10 @@
 #'
 #' @description `r lifecycle::badge("experimental")`
 #'
-#' [`CrmPackClass`] is a vertual class, from which all other `crmPack` classes
+#' [`CrmPackClass`] is a virtual class, from which all other `crmPack` classes
 #' inherit.
 #' @rdname CrmPackClass
+#' @aliases CrmPackClass
 #' @export
 #'
 .CrmPackClass <- setClass(Class = "CrmPackClass")
