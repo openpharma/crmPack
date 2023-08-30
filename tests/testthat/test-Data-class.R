a7f6b7ceabb89c457159b923d0c7a14326c454e3
Puzzled-Face <john.kirkpatrick@roche.com>
2023-08-30 14:48:01 +0000

    Fixing incorrect conflict resolution.

--- tests/testthat/test-Data-class.R
+++ tests/testthat/test-Data-class.R
@@ -235,6 +235,10 @@ test_that("DataGrouped object can be created with custom values", {
   result <- expect_silent(
     DataGrouped(
       group = c("mono", "combo", "mono"),
+      x = c(0.1, 0.5, 1.5),
+      y = c(0, 0, 0),
+      ID = 1:3,
+      cohort = 1:3,
       doseGrid = c(0.1, 0.5, 1.5, 3, 6, seq(from = 10, to = 80, by = 2))
     )
   )
