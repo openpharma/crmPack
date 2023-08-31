100097b898cdecc111970fb4c101d897db65817b
Puzzled-Face <john.kirkpatrick@roche.com>
2023-08-31 07:18:53 +0000

    Still trying to resolve pkgdown error

--- R/Data-methods.R
+++ R/Data-methods.R
@@ -380,7 +380,7 @@ setMethod(
 #'
 #' @return The new, updated [`DataOrdinal`] object.
 #'
-#' @aliases update-DataOrdinal
+#' @rdname update-DataOrdinal
 #' @export
 #' @example examples/DataOrdinal-method-update.R
 #'
