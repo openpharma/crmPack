2ff52758b0feefcb5984cbc1483460e0bbf3a6fb
Prerana Chandratre <prerana.chandratre@bayer.com>
2023-09-01 14:25:02 +0200

    small change: additional stat summary

--- R/Design-methods.R
+++ R/Design-methods.R
@@ -190,7 +190,9 @@ getResultList <- function(fun,
 ##' @param nCores how many cores should be used for parallel computing?
 ##' Defaults to the number of cores on the machine, maximum 5.
 ##' @param \dots not used
-##' @param derive A list of functions that derive statistics from the target dose samples.
+##' @param derive a list of functions which derives statistics, based on the
+##' vector of posterior MTD samples. Each list element must therefore accept
+##' one and only one argument, which is a numeric vector, and return a number.
 ##'
 ##' @return an object of class \code{\linkS4class{Simulations}}
 ##'
@@ -706,7 +708,9 @@ setMethod("simulate",
 ##' @param nCores how many cores should be used for parallel computing?
 ##' Defaults to the number of cores on the machine, maximum 5.
 ##' @param \dots not used
-##' @param derive A list of functions that derive statistics from the target dose samples.
+##' @param derive a list of functions which derives statistics, based on the
+##' vector of posterior MTD samples. Each list element must therefore accept
+##' one and only one argument, which is a numeric vector, and return a number.
 ##'
 ##' @return an object of class \code{\linkS4class{DualSimulations}}
 ##'
@@ -4179,7 +4183,9 @@ setMethod("simulate",
 ##' @param nCores how many cores should be used for parallel computing?
 ##' Defaults to the number of cores on the machine (maximum 5)
 ##' @param \dots not used
-##' @param derive A list of functions that derive statistics from the target dose samples.
+##' @param derive a list of functions which derives statistics, based on the
+##' vector of posterior MTD samples. Each list element must therefore accept
+##' one and only one argument, which is a numeric vector, and return a number.
 ##'
 ##' @return an object of class \code{\linkS4class{Simulations}}
 ##'
