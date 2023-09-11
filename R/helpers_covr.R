578d1abf5ecad929927e9e4428fcb1e63c58c739
dgkf <18220321+dgkf@users.noreply.github.com>
2023-09-11 14:13:56 -0400

    using identical for expression comparisons

--- R/helpers_covr.R
+++ R/helpers_covr.R
@@ -55,9 +55,10 @@ h_covr_detrace <- function(expr) {
 
   detrace <- function(x) {
     # returns "missing" expression to avoid errors with calls
-    if (x == bquote()) {
+    if (identical(x, bquote())) {
       return(x)
     }
+
     x <- h_covr_detrace_call(x)
     if (is.call(x)) x[-1] <- lapply(x[-1], h_covr_detrace)
     x
