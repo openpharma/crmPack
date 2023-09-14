153a71a434809a3e3bed5e877fca6882c738e5db
John Kirkpatrick <133956382+Puzzled-Face@users.noreply.github.com>
2023-09-14 16:54:39 +0100

    Implementing the DataOrdinal class (#667)
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: cicdguy <26552821+cicdguy@users.noreply.github.com>

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -162,6 +162,19 @@ h_get_data_sr_2 <- function() {
   )
 }
 
+# Sample ordinal data ----
+h_get_data_ordinal <- function() {
+  DataOrdinal(
+    x = c(10, 20, 30, 40, 50, 50, 50, 60, 60, 60),
+    y = as.integer(c(0, 0, 0, 0, 0, 1, 0, 0, 1, 2)),
+    ID = 1L:10L,
+    cohort = as.integer(c(1:4, 5, 5, 5, 6, 6, 6)),
+    doseGrid = c(seq(from = 10, to = 100, by = 10)),
+    yCategories = c("No tox" = 0L, "Sub-tox AE" = 1L, "DLT" = 2L),
+    placebo = FALSE
+  )
+}
+
 # DataGrouped ----
 
 h_get_data_grouped <- function(empty = FALSE, placebo = TRUE) {
