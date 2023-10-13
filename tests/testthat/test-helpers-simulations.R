f2205cb1a1814f1eab883c905c957f09045a2dec
John Kirkpatrick <133956382+Puzzled-Face@users.noreply.github.com>
2023-10-13 08:52:32 +0100

    Refactor myBarplot (#703)
    * Refactor myBarplot
    
    * [skip actions] Restyle files
    
    * Responding to lintr complaints
    
    * Rsponding to review comments
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ tests/testthat/test-helpers-simulations.R
@@ -0,0 +1,36 @@
+test_that("h_barplot_percentages produces consistent results", {
+  set.seed(504409)
+
+  plot <- h_barplot_percentages(
+    rpois(n = 100, lambda = 2),
+    "test"
+  )
+
+  vdiffr::expect_doppelganger("barplot-percentages", plot)
+})
+
+test_that("barplot_percentages fails gracefully with bad input", {
+  expect_error(
+    h_barplot_percentages(
+      as.character(sample(LETTERS, replace = TRUE, size = 100)),
+      "test"
+    ),
+    "Assertion on 'x' failed: Must be of type 'numeric', not 'character'."
+  )
+  expect_error(
+    h_barplot_percentages(rpois(n = 100, lambda = 2), "test", -3),
+    "Assertion on 'xaxisround' failed: Element 1 is not >= 0."
+  )
+  expect_error(
+    h_barplot_percentages(rpois(n = 100, lambda = 2), "test", "bad"),
+    "Assertion on 'xaxisround' failed: Must be of type 'number', not 'character'."
+  )
+  expect_error(
+    h_barplot_percentages(rpois(n = 100, lambda = 2), c("test", "oops")),
+    "Assertion on 'description' failed: Must have length 1, but has length 2."
+  )
+  expect_error(
+    h_barplot_percentages(rpois(n = 100, lambda = 2), 99),
+    "Assertion on 'description' failed: Must be of type 'character', not 'double'."
+  )
+})
