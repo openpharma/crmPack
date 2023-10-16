1fdb628c3deb526724c1eafde15b7f79084c4730
Puzzled-Face <john.kirkpatrick@roche.com>
2023-10-16 10:48:26 +0000

    lintr checks

--- R/helpers_rules.R
+++ R/helpers_rules.R
@@ -419,7 +419,7 @@ h_next_best_tdsamples_plot <- function(dose_target_drt_samples,
     aes(x = .data$TD, colour = .data$period),
   ) +
     geom_density(
-      aes(fill = .data$period,colour = .data$period),
+      aes(fill = .data$period, colour = .data$period),
       alpha = 0.25,
       bounds = dose_grid_range,
       show.legend = FALSE
@@ -434,65 +434,6 @@ h_next_best_tdsamples_plot <- function(dose_target_drt_samples,
     scale_fill_manual(
       values = c("during" = "orange", "end" = "violet")
     )
-
-
-
-
-    # scale_fill_manual(values = c(during = "orange", end = "violet")) +
-    # scale_color_manual(values = c(during = "orange", end = "violet")) +
-    # theme(legend.position = "none") +
-    # geom_vline(data = NULL, xintercept = dose_target_drt, colour = "orange") +
-    # annotate(
-    #   geom = "text",
-    #   label = paste("TD", nextBest@prob_target_drt * 100, "Estimate"),
-    #   x = dose_target_drt,
-    #   y = 0,
-    #   hjust = -0.1,
-    #   vjust = -20,
-    #   size = 5,
-    #   colour = "orange"
-    # ) +
-    # geom_text(
-    #   aes(
-    #     x = dose_target_drt + (dose_grid_range[2] - dose_grid_range[1]) * 0.05,
-    #     y = 0.010,
-    #     label = paste("TD", nextBest@prob_target_drt * 100, "Estimate"),
-    #     colour = "orange"
-    #   )
-    # ) +
-    # geom_vline(xintercept = dose_target_eot, colour = "violet") +
-    # annotate(
-    #   geom = "text",
-    #   label = paste("TD", nextBest@prob_target_eot * 100, "Estimate"),
-    #   x = dose_target_eot,
-    #   y = 0,
-    #   hjust = -0.1,
-    #   vjust = -25,
-    #   size = 5,
-    #   colour = "violet"
-    # )
-
-  # maxdoselimit <- min(doselimit, dose_grid_range[2])
-
-  # p +
-  #   geom_vline(xintercept = maxdoselimit, colour = "red", lwd = 1.1) +
-  #   geom_text(
-  #     data = data.frame(x = maxdoselimit),
-  #     aes(x, 0, label = "Max", hjust = +1, vjust = -35),
-  #     angle = 90,
-  #     vjust = 1.5,
-  #     hjust = 0.5,
-  #     colour = "red"
-  #   ) +
-  #   geom_vline(xintercept = next_dose, colour = "blue", lwd = 1.1) +
-  #   geom_text(
-  #     data = data.frame(x = next_dose),
-  #     aes(x, 0, label = "Next", hjust = 0.1, vjust = -30),
-  #     angle = 90,
-  #     vjust = -0.5,
-  #     hjust = 0.5,
-  #     colour = "blue"
-  #   )
 }
 
 #' Building the Plot for `nextBest-NextBestTD` Method.
