956a6ee2383abb1a99ec178d90573171aabedf95
dimitriskontosBAY <dimitris.kontos@bayer.com>
2023-11-01 07:16:02 +0100

    replaced stopifno(test_probability) with assert_probability

--- R/fromQuantiles.R
+++ R/fromQuantiles.R
@@ -76,8 +76,10 @@ Quantiles2LogisticNormal <- function(dosegrid,
                                        )) {
   ## extracts and checks
   nDoses <- length(dosegrid)
+
   assert_flag(logNormal)
   assert_flag(verbose)
+  assert_probability(level, bounds_closed = FALSE)
   stopifnot(
     !is.unsorted(dosegrid, strictly = TRUE),
     ## the medians must be monotonically increasing:
@@ -87,7 +89,6 @@ Quantiles2LogisticNormal <- function(dosegrid,
     identical(length(upper), nDoses),
     all(lower < median),
     all(upper > median),
-    test_probability(level, bounds = FALSE),
     identical(length(parlower), 5L),
     identical(length(parupper), 5L),
     all(parlower < parstart),
@@ -234,10 +235,8 @@ Quantiles2LogisticNormal <- function(dosegrid,
 ##' @export
 ##' @keywords programming
 getMinInfBeta <- function(p, q) {
-  stopifnot(
-    test_probability(p, bounds = FALSE),
-    test_probability(q, bounds = FALSE)
-  )
+  assert_probability(p, bounds_closed = FALSE)
+  assert_probability(q, bounds_closed = FALSE)
 
   ret <-
     if (q > p) {
@@ -309,12 +308,13 @@ MinimalInformative <- function(dosegrid,
                                ...) {
   ## extracts and checks
   nDoses <- length(dosegrid)
+
+  assert_probability(threshmin, bounds_closed = FALSE)
+  assert_probability(threshmax, bounds_closed = FALSE)
+  assert_probability(probmin, bounds_closed = FALSE)
+  assert_probability(probmax, bounds_closed = FALSE)
   stopifnot(
-    !is.unsorted(dosegrid, strictly = TRUE),
-    test_probability(threshmin, bounds = FALSE),
-    test_probability(threshmax, bounds = FALSE),
-    test_probability(probmin, bounds = FALSE),
-    test_probability(probmax, bounds = FALSE)
+    !is.unsorted(dosegrid, strictly = TRUE)
   )
   xmin <- dosegrid[1]
   xmax <- dosegrid[nDoses]
