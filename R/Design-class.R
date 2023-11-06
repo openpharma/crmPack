f26b0932a86246f8a2735934e9fd79d5641d4616
pengguanya <guanya.peng24@gmail.com>
2023-11-06 23:07:59 +0100

    Update R/Design-class.R
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>

--- R/Design-class.R
+++ R/Design-class.R
@@ -758,7 +758,7 @@ DualResponsesDesign <- function(eff_model,
 #' @slot safetyWindow (`SafetyWindow`)\cr the safety window to apply between cohorts.
 #'
 #' @details
-#' The `safetyWindow` slot should be an instance of the `SafetyWindow` class, which can be customized to specify the duration of the safety window for your trial. The safety window represents the time period during which the toxicity data is collected and analyzed to make dose escalation decisions.
+#' The `safetyWindow` slot should be an instance of the `SafetyWindow` class, which can be customized to specify the duration of the safety window for your trial. The safety window represents the time period required to observe toxicity data from the ongoing cohort before opening the next cohort. Note that even after opening the next cohort, further toxicity data will be collected and analyzed to make dose escalation decisions.
 #'
 #' To specify a constant safety window, use the `SafetyWindowConst` constructor. For example:
 #'
