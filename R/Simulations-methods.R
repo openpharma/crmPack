dbb371861d6b5debdf02b422bd69dc2b7d48eb34
Clara <clara.beck@bayer.com>
2023-12-04 13:18:49 +0000

    debugging git pipelines

--- R/Simulations-methods.R
+++ R/Simulations-methods.R
@@ -990,18 +990,6 @@ setMethod("show",
         param_names <- h_summarize_add_stats(stats_list = object@additional_stats)[[1]]
         averages <- h_summarize_add_stats(stats_list = object@additional_stats)[[2]]
 
-        # Access the nested list of values
-        # stats_list <- object@additional_stats
-
-        # Extract the parameter names
-        # param_names <- names(stats_list[[1]])
-
-        # Calculate the average for each parameter
-        #   averages <- lapply(param_names, function(param) {
-        #      values <- sapply(stats_list, function(x) x[[param]])
-        #      mean(values)
-        #    })
-
         for (i in seq_along(param_names)) {
           cat(param_names[i], ":", round(averages[[i]], 2), "\n")
         }
@@ -1999,14 +1987,14 @@ setMethod("show",
 
       # Report results of additional statistics summary
 
-      if (length(unlist(object@additional_stats)) > 0) {
-        param_names <- h_summarize_add_stats(object@additional_statistics)[1]
-        averages <- h_summarize_add_stats(object@additional_statistics)[2]
+   #   if (length(unlist(object@additional_stats)) > 0) {
+  #      param_names <- h_summarize_add_stats(object@additional_statistics)[1]
+  #      averages <- h_summarize_add_stats(object@additional_statistics)[2]##
 
-        for (i in seq_along(param_names)) {
-          cat(param_names[i], ":", round(averages[[i]], 2), "\n")
-        }
-      }
+    #    for (i in seq_along(param_names)) {
+    #      cat(param_names[i], ":", round(averages[[i]], 2), "\n")
+    #    }
+    #  }
 
       ## finally assign names to the df
       ## and return it invisibly
