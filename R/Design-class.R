8b104a664afd700200790365ae6bfd7f904aadfc
Puzzled-Face <john.kirkpatrick@roche.com>
2023-12-12 15:20:49 +0000

    Implement RuleDesignOrdinal and DesignOrdinal classes

--- R/Design-class.R
+++ R/Design-class.R
@@ -989,3 +989,208 @@ DesignGrouped <- function(model,
 #' @note Typically, end-users will not use the `.DefaultDesignGrouped()` function.
 #' @export
 .DefaultDesignGrouped <- .DesignGrouped
+
+# RuleDesignOrdinal ----
+
+## class ----
+
+#' `RuleDesignOrdinal`
+#'
+#' @description `r lifecycle::badge("experimental")`
+#'
+#' [`RuleDesignOrdinal`] is the class for rule-based designs. The difference between
+#' this class and the [`DesignOrdinal`] class is that [`RuleDesignOrdinal`]
+#' does not contain `model`, `stopping` and `increments` slots.
+#'
+#' @slot next_best (`NextBest`)\cr how to find the next best dose.
+#' @slot cohort_size (`CohortSize`)\cr rules for the cohort sizes.
+#' @slot data (`Data`)\cr specifies dose grid, any previous data, etc.
+#' @slot starting_dose (`number`)\cr the starting dose, it must lie on the dose
+#'   grid in `data`.
+#'
+#' @aliases RuleDesignOrdinal
+#' @export
+#'
+.RuleDesignOrdinal <- setClass(
+  Class = "RuleDesignOrdinal",
+  slots = c(
+    next_best = "NextBestOrdinal",
+    cohort_size = "CohortSizeOrdinal",
+    data = "DataOrdinal",
+    starting_dose = "numeric"
+  ),
+  prototype = prototype(
+    next_best = .NextBestOrdinal(),
+    cohort_size = CohortSizeOrdinal(1L, CohortSizeConst(3L)),
+    data = DataOrdinal(doseGrid = 1:3),
+    starting_dose = 1
+  ),
+  contains = "CrmPackClass",
+  validity = v_rule_design_ordinal
+)
+
+## constructor ----
+
+#' @rdname RuleDesignOrdinal-class
+#'
+#' @param next_best (`NextBestOrdinal`)\cr see slot definition.
+#' @param cohort_size (`CohortSizeOrdinal`)\cr see slot definition.
+#' @param data (`DataOrdinal`)\cr see slot definition.
+#' @param starting_dose (`number`)\cr see slot definition.
+#'
+#' @export
+#' @example examples/Design-class-RuleDesignOrdinal.R
+#'
+RuleDesignOrdinal <- function(
+    next_best,
+    cohort_size,
+    data,
+    starting_dose) {
+  new(
+    "RuleDesignOrdinal",
+    next_best = next_best,
+    cohort_size = cohort_size,
+    data = data,
+    starting_dose = as.numeric(starting_dose)
+  )
+}
+
+#' @rdname RuleDesignOrdinal-class
+#' @note Typically, end users will not use the `.DefaultRuleDesignOrdinal()` function.
+#' @export
+
+.DefaultRuleDesignOrdinal <- function() {
+  RuleDesignOrdinal(
+    next_best = CohortSizeOrdinal(1L, NextBestMTD(target = 0.25)),
+    cohort_size = CohortSizeOrdinal(1L, CohortSizeConst(size = 3L)),
+    data = DataOrdinal(doseGrid = c(5, 10, 15, 25, 35, 50, 80)),
+    starting_dose = 5
+  )
+}
+
+# DesignOrdinal ----
+
+## class ----
+
+#' `DesignOrdinal`
+#'
+#' @description `r lifecycle::badge("experimental")`
+#'
+#' [`DesignOrdinal`] is the class for rule-based ordinal designs. The difference
+#' between this class and its parent [`RuleDesignOrdinal`] class is that the
+#'  [`DesignOrdinal`] class contains additional `model`, `stopping` and
+#'  `increments` slots.
+#'
+#' @slot model (`LogisticLogNormalOrdinal`)\cr the model to be used.
+#' @slot stopping (`StoppingOrdinal`)\cr stopping rule(s) for the trial.
+#' @slot increments (`IncrementsOrdinal`)\cr how to control increments between dose levels.
+#' @slot pl_cohort_size (`CohortSizeOrdinal`)\cr rules for the cohort sizes for placebo,
+#'   if any planned (defaults to constant 0 placebo patients).
+#'
+#' @aliases DesignOrdinal
+#' @export
+#'
+.DesignOrdinal <- setClass(
+  Class = "DesignOrdinal",
+  slots = c(
+    model = "LogisticLogNormalOrdinal",
+    stopping = "StoppingOrdinal",
+    increments = "IncrementsOrdinal",
+    pl_cohort_size = "CohortSizeOrdinal"
+  ),
+  prototype = prototype(
+    model = .LogisticLogNormalOrdinal(),
+    next_best = .NextBestOrdinal(),
+    stopping = .StoppingOrdinal(),
+    increments = .IncrementsOrdinal(),
+    pl_cohort_size = CohortSizeOrdinal(1L, CohortSizeConst(3L))
+  ),
+  contains = "RuleDesignOrdinal"
+)
+
+## constructor ----
+
+#' @rdname DesignOrdinal-class
+#'
+#' @param model (`LogisticLogNormalOrdinal`)\cr see slot definition.
+#' @param stopping (`StoppingOrdinal`)\cr see slot definition.
+#' @param increments (`IncrementsOrdinal`)\cr see slot definition.
+#' @param pl_cohort_size (`CohortSizeOrdinal`)\cr see slot definition.
+#' @inheritDotParams RuleDesignOrdinal
+#'
+#' @export
+#' @example examples/Design-class-DesignOrdinal.R
+#'
+#'
+DesignOrdinal <- function(
+    model,
+    stopping,
+    increments,
+    pl_cohort_size = CohortSizeOrdinal(1L, CohortSizeConst(0L)),
+    ...) {
+  start <- RuleDesignOrdinal(...)
+  new(
+    "DesignOrdinal",
+    start,
+    model = model,
+    stopping = stopping,
+    increments = increments,
+    pl_cohort_size = pl_cohort_size
+  )
+}
+
+## default constructor ----
+
+#' @rdname DesignOrdinal-class
+#' @note Typically, end users will not use the `.DefaultDesignOrdinal()` function.
+#' @export
+.DefaultDesignOrdinal <- function() {
+  my_size1 <- CohortSizeRange(
+    intervals = c(0, 30),
+    cohort_size = c(1, 3)
+  )
+  my_size2 <- CohortSizeDLT(
+    intervals = c(0, 1),
+    cohort_size = c(1, 3)
+  )
+  my_size <- CohortSizeOrdinal(1L, maxSize(my_size1, my_size2))
+
+  my_stopping1 <- StoppingMinCohorts(nCohorts = 3)
+  my_stopping2 <- StoppingTargetProb(
+    target = c(0.2, 0.35),
+    prob = 0.5
+  )
+  my_stopping3 <- StoppingMinPatients(nPatients = 20)
+  my_stopping <- StoppingOrdinal(1L, (my_stopping1 & my_stopping2) | my_stopping3)
+
+  # Initialize the design.
+  design <- DesignOrdinal(
+    model = LogisticLogNormalOrdinal(
+      mean = c(-0.85, 1),
+      cov = matrix(c(1, -0.5, -0.5, 1), nrow = 2),
+      ref_dose = 56
+    ),
+    next_best = NextBestOrdinal(
+      1L,
+      NextBestNCRM(
+        target = c(0.2, 0.35),
+        overdose = c(0.35, 1),
+        max_overdose_prob = 0.25
+      )
+    ),
+    stopping = my_stopping,
+    increments = IncrementsOrdinal(
+      1L,
+      IncrementsRelative(
+        intervals = c(0, 20),
+        increments = c(1, 0.33)
+      )
+    ),
+    cohort_size = my_size,
+    data = DataOrdinal(
+      doseGrid = c(1, 3, 5, 10, 15, 20, 25, 40, 50, 80, 100),
+      yCategories = c("No tox" = 0L, "Sub-tox AE" = 1L, "DLT" = 2L)
+    ),
+    starting_dose = 3
+  )
+}
