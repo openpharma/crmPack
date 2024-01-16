ec269fb58a18f4ced6fd917b871276a8a1c9a2c9
Puzzled-Face <john.kirkpatrick@roche.com>
2024-01-16 15:27:03 +0000

    Caching progress

--- R/helpers_knitr_CohortSize.R
+++ R/helpers_knitr_CohortSize.R
@@ -13,21 +13,24 @@ NULL
 
 # CohortSize ----
 
-# [1]  "CohortSizeParts"   "CohortSizeMax"     "CohortSizeMin"
+# [1]  "CohortSizeMax"     "CohortSizeMin"
 # [7] "CohortSizeOrdinal"
 
 #' Render a CohortSizeConst Object
 #'
 #' @description `r lifecycle::badge("experimental")`
-#' @param obj (`numeric`)\cr The CohortSizeDLT object.
-#' @param asis (`flag`)\cr Should the return value be wrapped in a call to `asis_output`? (Not used at present)
+#' @param obj (`CohortSize`)\cr The object to knit_print.
+#' @param asis (`flag`)\cr Should the return value be wrapped in a call to `asis_output`?
 #' @param label (`character`)\cr The word used to label the participants.  See Usage Notes below.
-#' @param ... (`numeric`)\cr Not used at present
+#' @param ... Not used at present
 #'
 #' @section Usage Notes:
-#' The default value of `label` is `NULL` and results in a label of `"participant"` if the cohort size is `1` and
-#' `"participants"` otherwise.  In other cases, the end user is responsible for ensuring consistency of number
-#' between the cohort size and its label
+#' `label` describes the trial's participants.
+#'
+#' It should be a character vector of length 1 or 2.  If of length 2, the first
+#' element describes a `cohort_size` of 1 and the second describes all other
+#' `cohort_size`s.  If of length 1, the character `s` is appended to the value
+#' when `cohort_size` is not 1.
 #' @return The markdown representation of the object, as a character string
 #' @seealso [`knit_print`] for more details.
 #'
@@ -37,21 +40,18 @@ knit_print.CohortSizeConst <- function(obj, asis = TRUE, label = c("participant"
   assert_character(label, min.len = 1, max.len = 2, any.missing = FALSE)
   assert_flag(asis)
 
-  paste0("A constant size of ", obj@size, " ", label)
+  if (length(label) == 1) {
+    label[2] <- paste0(label[1], "s")
+  }
+  paste0("A constant size of ", obj@size, " ", label[obj@size == 1 ? 1 : 2])
 }
 
 #' Render a CohortSizeRange Object
 #'
 #' @description `r lifecycle::badge("experimental")`
-#' @param obj (`numeric`)\cr The CohortSizeDLT object.
-#' @param asis (`flag`)\cr Should the return value be wrapped in a call to `asis_output`?
-#' @param ... (`numeric`)\cr Passed to `knitr::kable()`
-#'
-#' @section Usage Notes:
-#' The by default, the columns are labelled `Lower`, `Upper` and `Cohort size`.  The table's caption is
-#' `Defined by the dose to be used in the next cohort`.  These values can be overridden by passing
-#' `col.names` and `caption` in the function call.
-#' @return The markdown representation of the object, as a character string
+#' @inheritParams knit_print.CohortSizeConst
+#' @inheritDotParam knit_print.CohortSizeConst
+#' @inherit knit_print.CohortSizeConst return
 #'
 #' @export
 #' @rdname knit_print
@@ -78,16 +78,15 @@ knit_print.CohortSizeRange <- function(object, asis = TRUE, ...) {
 #' Render a CohortSizeDLT Object
 #'
 #' @description `r lifecycle::badge("experimental")`
-#' @param obj (`numeric`)\cr The CohortSizeDLT object.
-#' @param asis (`flag`)\cr Should the return value be wrapped in a call to `asis_output`?
+#' @inheritParams knit_print.CohortSizeConst
+#' @inheritDotParam knit_print.CohortSizeConst
+#' @inherit knit_print.CohortSizeConst return
 #' @param ... (`numeric`)\cr Passed to `knitr::kable`.
 #'
 #' @section Usage Notes:
 #' The by default, the columns are labelled `Lower`, `Upper` and `Cohort size`.  The table's caption is
 #' `Defined by the number of DLTs so far observed`.  These values can be overridden by passing
 #' `col.names` and `caption` in the function call.
-#'
-#' @return The markdown representation of the object, as a character string
 #'
 #' @export
 #' @rdname knit_print
@@ -114,18 +113,30 @@ knit_print.CohortSizeDLT <- function(object, asis = TRUE, ...) {
 #' Render a CohortSizeParts Object
 #'
 #' @description `r lifecycle::badge("experimental")`
-#' @param obj (`numeric`)\cr The CohortSizeParts object.
-#' @param asis (`flag`)\cr Should the return value be wrapped in a call to `asis_output`?
-#' @param ... (`numeric`)\cr Passed to `knitr::kable`.
-#'
-#' @return The markdown representation of the object, as a character string
+#' @inheritParams knit_print.CohortSizeConst
+#' @inherit knit_print.CohortSizeConst return
+#' @inheritSection knit_print.CohortSizeConst {Usage Notes}
+#' @inheritDotParams knit_print.CohortSizeConst
 #'
 #' @export
 #' @rdname knit_print
-knit_print.CohortSizeParts <- function(object, asis = TRUE, ...) {
+knit_print.CohortSizeParts <- function(obj, label = "participant", asis = TRUE, ...) {
+  assert_character(label, min.len = 1, max.len = 2, any.missing = FALSE)
   assert_flag(asis)
-
-  rv <- paste0("A constant size of ", obj@size, " ", label)
+  if (length(label) == 1) {
+    label[2] <- paste0(label[1], "s")
+  }
+  rv <- paste0(
+    "A size of ",
+    obj@cohort_sizes[1],
+    " ",
+    label[ifelse(obj@cohort_sizes[1] == 1, 1, 2)],
+    " in the first part and ",
+    obj@cohort_sizes[2],
+    " ",
+    label[ifelse(obj@cohort_sizes[2] == 1, 1, 2)],
+    " in the second."
+  )
   if (asis) {
     rv <- asis_output(rv)
   }
