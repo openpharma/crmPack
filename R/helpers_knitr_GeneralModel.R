8a40ee59f98333c04a0468232040d1f8241ec14b
Puzzled-Face <john.kirkpatrick@roche.com>
2024-02-19 14:44:25 +0000

    knit_print for the first six model classes

---
+++ R/helpers_knitr_GeneralModel.R
@@ -0,0 +1,372 @@
+#' Render the Parameters of a Normal Distribution
+#'
+#' @description `r lifecycle::badge("experimental")`
+#' @keywords internal
+h_knit_print_render_model_params <- function(x, use_values = TRUE, fmt = "%5.2f", ...) {
+  assert_class(x, "ModelParamsNormal")
+  assert_true(length(x@mean) == 2)
+
+  muAlpha <- ifelse(
+    use_values,
+    sprintf(fmt, x@mean[1]),
+    "\\mu~\\alpha~"
+  )
+  muBeta <- ifelse(
+    use_values,
+    sprintf(fmt, x@mean[2]),
+    "\\mu;~\\beta~"
+  )
+  sigma11 <- ifelse(
+    use_values,
+    sprintf(fmt, x@cov[1, 1]),
+    "\\sigma_\\alpha\\sigma_\\alpha"
+  )
+  sigma22 <- ifelse(
+    use_values,
+    sprintf(fmt, x@cov[2, 2]),
+    "\\sigma_\\beta\\sigma_\\beta"
+  )
+  sigma12 <- ifelse(
+    use_values,
+    sprintf(fmt, x@cov[1, 2]),
+    "\\rho\\sigma_\\alpha\\sigma_\\beta"
+  )
+  sigma21 <- ifelse(
+    use_values,
+    sprintf(fmt, x@cov[2, 1]),
+    "\\rho\\sigma_\\beta\\sigma_\\alpha"
+  )
+  paste0(
+    "N \\left(\\begin{bmatrix}", muAlpha, " \\\\ ", muBeta, "\\end{bmatrix} , ",
+    "\\begin{bmatrix*}[S] ", sigma11, " & ", sigma12, " \\\\ ", sigma21, " & ", sigma22,
+    "\\end{bmatrix*} \\right)"
+  )
+}
+
+knit_print.ModelParamsNormal <- function(x, ..., asis = TRUE, use_values = TRUE, fmt = "%5.2f") {
+  result <- paste0(
+    "The prior for &theta; is given by\\n",
+    "$$ \\theta = \\begin{bmatrix*}[S] \\alpha \\\\ log(\\beta) \\end{bmatrix*}",
+    " \\sim ",
+    h_knit_print_render_model_params(x),
+    " $$"
+  )
+  if (asis) {
+    knitr::asis_output(result)
+  }
+  result
+}
+
+registerS3method(
+  "knit_print",
+  "ModelParamsNormal",
+  knit_print.ModelParamsNormal
+)
+
+#' @keywords internal
+knit_print.GeneralModel <- function(x, ..., asis = TRUE, use_values = TRUE, fmt = "%5.2f", units = NA) {
+  # Validate
+  assert_flag(asis)
+  assert_flag(use_values)
+  assert_character(
+    fmt,
+    len = 1,
+    any.missing = FALSE,
+    # https://stackoverflow.com/questions/446285/validate-sprintf-format-from-input-field-with-regex
+    pattern = "%(?:\\d+\\$)?[+-]?(?:[ 0]|'.{1})?-?\\d*(?:\\.\\d+)?[bcdeEufFgGosxX]",
+  )
+  assert_character(units, len = 1)
+  # Initialise
+  if (is.na(units)) {
+    units <- ""
+  } else {
+    units <- paste0(" ", units)
+  }
+  # Execute
+  ref_dose <- ifelse(
+    use_values,
+    paste0(
+      " The reference dose will be ",
+      stringr::str_trim(sprintf(fmt, x@ref_dose)),
+      units,
+      "."
+    )
+  )
+  rv <- paste0(
+    h_knit_print_render_model(x, use_values = use_values, fmt = fmt),
+    knit_print(x@params, ..., asis = asis, use_vales = use_values, fmt = fmt),
+    "\\n\\n",
+    ref_dose,
+    "\\n\\n"
+  )
+  if (asis) {
+    rv <- knitr::asis_output(rv)
+  }
+  rv
+}
+
+registerS3method(
+  "knit_print",
+  "GeneralModel",
+  knit_print.GeneralModel
+)
+
+# ModelLogNormal
+# LogisticLogNormalSub           Done
+# LogisticKadane
+# LogisticNormalMixture
+# LogisticNormalFixedMixture
+# DualEndpoint
+# OneParLogNormalPrior
+# OneParExpPrior
+# LogisticNormal                 Done
+# LogisticLogNormal              Done
+# ProbitLogNormal                Done
+# ProbitLogNormalRel
+# LogisticLogNormalGrouped
+# LogisticKadaneBetaGamma
+# DualEndpointRW
+# DualEndpointBeta
+# DualEndpointEmax
+# LogisticLogNormalMixture
+# DALogisticLogNormal
+# TITELogisticLogNormal
+# FractionalCRM
+# LogisticLogNormalOrdinal
+
+#' Render a Model Function in a `knit_print` Method
+#'
+#' This is a helper method used `knit_print` for `crmPack` classes.
+#'
+#' @param x (`GeneralModel`)\cr the model object that will be printed
+#' @param ...\cr Not used at present
+#' @return A character string containing a LaTeX rendition of the model.
+#' @noRd
+h_knit_print_render_model <- function(x, ...) {
+  UseMethod("h_knit_print_render_model")
+}
+
+#' @description `r lifecycle::badge("experimental")`
+#' @noRd
+h_knit_print_render_model.LogisticLogNormal <- function(x, ...) {
+  z <- "e^{\\alpha + \\beta \\cdot log(d/d_{ref})}"
+  paste0(
+    "A logistic log normal model will describe the relationship between dose and toxicity: ",
+    "$$ p(Tox | d) = f(X = 1 | \\theta, d) = \\frac{", z, "}{1 + ", z, "} $$\\n ",
+    "where d~ref~ denotes a reference dose.\n\n"
+  )
+}
+
+registerS3method(
+  "h_knit_print_render_model",
+  "LogisticLogNormal",
+  h_knit_print_render_model.LogisticLogNormal
+)
+
+#' @description `r lifecycle::badge("experimental")`
+#' @noRd
+h_knit_print_render_model.LogisticLogNormalSub <- function(x, ...) {
+  z <- "e^{\\alpha + \\beta \\cdot (d \\, - \\, d_{ref})}"
+  paste0(
+    "A logistic log normal model with subtractive dose normalisation will ",
+    "describe the relationship between dose and toxicity: \n\n",
+    "$$ p(Tox | d) = f(X = 1 | \\theta, d) = \\frac{", z, "}{1 + ", z, "} $$\\n ",
+    "where d~ref~ denotes a reference dose.\n\n"
+  )
+}
+
+registerS3method(
+  "h_knit_print_render_model",
+  "LogisticLogNormalSub",
+  h_knit_print_render_model.LogisticLogNormalSub
+)
+
+#' @description `r lifecycle::badge("experimental")`
+#' @noRd
+h_knit_print_render_model.LogisticNormal <- function(x, ...) {
+  z <- "e^{\\alpha + \\beta \\cdot d/d_{ref}}"
+  paste0(
+    "A logistic log normal model will describe the relationship between dose and toxicity: ",
+    "$$ p(Tox | d) = f(X = 1 | \\theta, d) = \\frac{", z, "}{1 + ", z, "} $$\\n ",
+    "where d~ref~ denotes a reference dose.\n\n"
+  )
+}
+registerS3method(
+  "h_knit_print_render_model",
+  "LogisticNormal",
+  h_knit_print_render_model.LogisticNormal
+)
+
+#' @description `r lifecycle::badge("experimental")`
+#' @noRd
+h_knit_print_render_model.ProbitLogNormal <- function(x, ...) {
+  paste0(
+    "A probit log normal model will describe the relationship between dose and toxicity: ",
+    "$$ \\Phi^{-1}(Tox | d) = f(X = 1 | \\theta, d) = \\alpha + \\beta \\cdot log(d/d_{ref}) $$\\n ",
+    "where d~ref~ denotes a reference dose.\n\n"
+  )
+}
+
+registerS3method(
+  "h_knit_print_render_model",
+  "ProbitLogNormal",
+  h_knit_print_render_model.ProbitLogNormal
+)
+
+#' @description `r lifecycle::badge("experimental")`
+#' @noRd
+h_knit_print_render_model.LogisticNormalMixture <- function(x, ...) {
+  z <- "e^{\\alpha + \\beta \\cdot log(d/d_{ref})}"
+  paste0(
+    "A mixture of two logistic log normal models will describe the relationship between dose and toxicity: ",
+    "$$ p(Tox | d) = f(X = 1 | \\theta, d) = \\frac{", z, "}{1 + ", z, "} $$\\n ",
+    "where d~ref~ denotes a reference dose.\n\n"
+  )
+}
+
+registerS3method(
+  "h_knit_print_render_model",
+  "LogisticNormalMixture",
+  h_knit_print_render_model.LogisticNormalMixture
+)
+
+#' @keywords internal
+knit_print.LogisticNormalMixture <- function(x, ..., asis = TRUE, use_values = TRUE, fmt = "%5.2f", units = NA) {
+  # Validate
+  assert_flag(asis)
+  assert_flag(use_values)
+  assert_character(
+    fmt,
+    len = 1,
+    any.missing = FALSE,
+    # https://stackoverflow.com/questions/446285/validate-sprintf-format-from-input-field-with-regex
+    pattern = "%(?:\\d+\\$)?[+-]?(?:[ 0]|'.{1})?-?\\d*(?:\\.\\d+)?[bcdeEufFgGosxX]",
+  )
+  assert_character(units, len = 1)
+  # Initialise
+  if (is.na(units)) {
+    units <- ""
+  } else {
+    units <- paste0(" ", units)
+  }
+  # Execute
+  ref_dose <- ifelse(
+    use_values,
+    paste0(
+      " The reference dose will be ",
+      stringr::str_trim(sprintf(fmt, x@ref_dose)),
+      units,
+      "."
+    )
+  )
+  rv <- paste0(
+    h_knit_print_render_model(x, use_values = use_values, fmt = fmt),
+    "The prior for &theta; is given by\\n",
+    "$$ \\theta = \\begin{bmatrix*}[S] \\alpha \\\\ log(\\beta) \\end{bmatrix*}",
+    " \\sim ",
+    "w \\cdot ",
+    h_knit_print_render_model_params(x@comp1),
+    " + (1 - w) \\cdot ",
+    h_knit_print_render_model_params(x@comp2),
+    " $$\\n\\n",
+    " and the prior for w is given by \n\n",
+    " $$ w \\sim Beta(", x@weightpar[1], ", ", x@weightpar[2], ") $$\\n\\n",
+    ref_dose,
+    "\\n\\n"
+  )
+  if (asis) {
+    rv <- knitr::asis_output(rv)
+  }
+  rv
+}
+
+registerS3method(
+  "knit_print",
+  "LogisticNormalMixture",
+  knit_print.LogisticNormalMixture
+)
+
+#' @keywords internal
+knit_print.LogisticNormalFixedMixture <- function(x, ..., asis = TRUE, use_values = TRUE, fmt = "%5.2f", units = NA) {
+  # Validate
+  assert_flag(asis)
+  assert_flag(use_values)
+  assert_character(
+    fmt,
+    len = 1,
+    any.missing = FALSE,
+    # https://stackoverflow.com/questions/446285/validate-sprintf-format-from-input-field-with-regex
+    pattern = "%(?:\\d+\\$)?[+-]?(?:[ 0]|'.{1})?-?\\d*(?:\\.\\d+)?[bcdeEufFgGosxX]",
+  )
+  assert_character(units, len = 1)
+  # Initialise
+  if (is.na(units)) {
+    units <- ""
+  } else {
+    units <- paste0(" ", units)
+  }
+  # Execute
+  ref_dose <- ifelse(
+    use_values,
+    paste0(
+      " The reference dose will be ",
+      stringr::str_trim(sprintf(fmt, x@ref_dose)),
+      units,
+      "."
+    )
+  )
+  rv <- paste0(
+    h_knit_print_render_model(x, use_values = use_values, fmt = fmt),
+    " The prior for &theta; is given by\\n\\n",
+    "$$ \\theta = \\begin{bmatrix*}[S] \\alpha \\\\ log(\\beta) \\end{bmatrix*}",
+    " \\sim \\sum_{i=1}^{", length(x@components),"}",
+    "w_i \\cdot N \\left( \\mathbf{\\mu}_i ,  \\mathbf{\\Sigma}_i \\right)",
+    " $$ \\n\\n",
+    " with \\n\\n",
+    "$$ \\sum_{i=1}^{", length(x@components),"} w_i = 1 $$ \\n\\n",
+    " The individual components of the mixture are "
+  )
+  for (i in seq_along(x@components)) {
+    comp <- x@components[[i]]
+    rv <- paste0(
+      rv,
+      " \\n\\n $$ ", h_knit_print_render_model_params(comp, use_values, fmt, asis = FALSE), " \\text{ with weight ", x@weights[i], "} $$ ",
+      ifelse(
+        i < length(x@components),
+        " and",
+        " "
+      )
+    )
+  }
+  rv <- paste0(rv, " \\n\\n ", ref_dose, "\\n\\n")
+  if (asis) {
+    rv <- knitr::asis_output(rv)
+  }
+  rv
+}
+
+registerS3method(
+  "knit_print",
+  "LogisticNormalFixedMixture",
+  knit_print.LogisticNormalFixedMixture
+)
+
+#' @description `r lifecycle::badge("experimental")`
+#' @noRd
+h_knit_print_render_model.LogisticNormalFixedMixture <- function(x, ...) {
+  z <- "e^{\\alpha + \\beta \\cdot log(d/d_{ref})}"
+  paste0(
+    "A mixture of ",
+    length(x@components),
+    " logistic log normal models with fixed weights will describe the relationship ",
+    "between dose and toxicity: ",
+    "$$ p(Tox | d) = f(X = 1 | \\theta, d) = \\frac{", z, "}{1 + ", z, "} $$\\n ",
+    "where d~ref~ denotes a reference dose.\n\n"
+  )
+}
+
+registerS3method(
+  "h_knit_print_render_model",
+  "LogisticNormalFixedMixture",
+  h_knit_print_render_model.LogisticNormalFixedMixture
+)
