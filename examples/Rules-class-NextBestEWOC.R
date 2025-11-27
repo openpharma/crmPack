9f72c323752a088c9ff029d786c75b7f479f6ee0
Daniel Sabanes Bove <daniel.sabanesbove@gmx.net>
2025-11-27 10:18:53 +0800

    avoid utf8 char

--- examples/Rules-class-NextBestEWOC.R
+++ examples/Rules-class-NextBestEWOC.R
@@ -1,6 +1,6 @@
 # Example: Define EWOC next best dose rule.
 # Target toxicity probability is 0.30. Overdose region is any probability > 0.35.
-# We restrict posterior probability of recommending an overdosing dose to ≤ 0.25.
+# We restrict posterior probability of recommending an overdosing dose to <= 0.25.
 next_best_ewoc <- NextBestEWOC(
   target = 0.30,
   overdose = c(0.35, 1),
