fbd65c5f9ae2fe4d17e19d62dfb86179bd5b7f41
Daniel Sabanes Bove <daniel.sabanesbove@gmx.net>
2025-12-04 20:52:25 +0800

    reduce mcmc iterations in stopTrial examples

--- examples/Rules-method-stopTrial-StoppingTargetBiomarker.R
+++ examples/Rules-method-stopTrial-StoppingTargetBiomarker.R
@@ -37,7 +37,7 @@ model <- DualEndpointRW(
 )
 
 # Set-up some MCMC parameters and generate samples from the posterior
-options <- McmcOptions(burnin = 100, step = 2, samples = 500)
+options <- McmcOptions(burnin = 10, step = 2, samples = 40)
 set.seed(94)
 samples <- mcmc(data, model, options)
 
