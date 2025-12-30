d7b159334e12a0d92632088371fb79cb696dbbfd
Daniel Sabanes Bove <daniel.sabanesbove@gmx.net>
2025-12-30 14:38:12 +0800

    be even smarter and adapt the output type based on the futile.logger package version, to avoid compatibility issues

--- tests/testthat/helper-logger.R
+++ tests/testthat/helper-logger.R
@@ -1,5 +1,14 @@
+log_trace_output_type <- if (packageVersion("futile.logger") >= "1.4.9") {
+  "message"
+} else {
+  "output"
+}
+
 h_test_logging_enabled <- function() {
-  log_msg <- capture.output(log_trace("test_that message"), type = "message")
+  log_msg <- capture.output(
+    log_trace("test_that message"),
+    type = log_trace_output_type
+  )
   verb <- is_logging_enabled()
   log1 <- grepl("TRACE", log_msg)
   log2 <- grepl("test_that message", log_msg)
@@ -8,7 +17,10 @@ h_test_logging_enabled <- function() {
 }
 
 h_test_logging_disabled <- function() {
-  log_msg <- capture.output(log_trace("test_that message"), type = "message")
+  log_msg <- capture.output(
+    log_trace("test_that message"),
+    type = log_trace_output_type
+  )
   verb <- is_logging_enabled()
   log1 <- length(log_msg) == 0L
 
